a9a693896748dd6bd02e5f65badc790cd5b9a974
Simon Tokumine <simon@japancentre.com>
2011-08-11 18:40:53 +0100

    base layout

---
+++ app.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/*
+* Windshaft loader
+* ===============
+*
+* ./app.js [environment]
+*
+* environments: [development, production] 
+*/
+
+var _ = require('underscore');
+
+// sanity check
+var ENV = process.argv[2]
+if (ENV != 'development' && ENV != 'production'){
+  console.error("\n./app.js [environment]");
+  console.error("environments: [development, production]\n");
+  process.exit(1);
+}
+
+// set environment specific variables 
+global.settings     = require(__dirname + '/config/settings')
+global.environment  = require(__dirname + '/config/environments/' + ENV)
+_.extend(global.settings, global.environment)
+ 
+// boot 
+require('./server');
+
+
+
+
+
---
+++ config/settings.js
@@ -0,0 +1,1 @@
+module.exports.oneDay   = 86400000;
