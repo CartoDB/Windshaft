a5f5e4524da2248bf2724e2e670f978916d34a20
Simon Tokumine <simon@japancentre.com>
2011-08-11 19:10:08 +0100

    get tests running

--- test/server.js
+++ test/server.js
@@ -6,3 +6,12 @@ var tests  = module.exports = {};
 tests['true'] = function() {
   assert.ok(true);
 }
+
+test["get style returns a default style"] = function(){
+  assert.response(server, {
+      url: '/',
+      method: 'GET'
+  },{
+      status: 200,
+  });
+};
