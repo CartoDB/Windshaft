975715517db8fb20cb538daafd305f2150dca52d
Simon Tokumine <simon@japancentre.com>
2011-09-01 02:01:08 +0100

    working render_cache class. Understood more about tilelive-mapnik internals

--- app.js
+++ app.js
@@ -7,23 +7,24 @@
 * environments: [development, production] 
 */
 
-var _ = require('underscore');
-var Windshaft = require('lib/windshaft');
-var serverOptions = require('lib/cartodb/server_options');
 
 // sanity check
 var ENV = process.argv[2]
 if (ENV != 'development' && ENV != 'production'){
-  console.error("\n./app.js [environment]");
+  console.error("\nnode app.js [environment]");
   console.error("environments: [development, production]\n");
   process.exit(1);
 }
 
-// set environment specific variables 
-global.settings     = require(__dirname + '/config/settings')
-global.environment  = require(__dirname + '/config/environments/' + ENV)
-_.extend(global.settings, global.environment)
+var _ = require('underscore');
 
+// set environment specific variables
+global.settings     = require(__dirname + '/config/settings');
+global.environment  = require(__dirname + '/config/environments/' + ENV);
+_.extend(global.settings, global.environment);
+
+var Windshaft = require('./lib/windshaft');
+var serverOptions = require('./lib/cartodb/server_options');
 
 // boot
 var ws = new Windshaft.Server(serverOptions);
---
+++ test/unit/windshaft/render_cache.test.js
@@ -0,0 +1,57 @@
+var   _         = require('underscore')
+    , th        = require('../../test_helper')
+    , assert    = require('assert')
+    , RenderCache = require('../../../lib/windshaft/render_cache')
+    , tests       = module.exports = {};
+
+
+tests['true'] = function() {
+    assert.eql(global.environment.name, 'test');
+};
+
+tests['render_cache has a cached of render objects'] = function(){
+    assert.ok(_.isObject(RenderCache.renderers));
+};
+
+tests['render_cache can create a unique key from request, stripping xyz/callback'] = function(){
+    var req = {params: {dbname: "vizzuality", table: 'my_table', x: 4, y:4, z:4, sql:"select *", geom_type:'point', format:'png' }};
+
+    assert.eql(RenderCache.createKey(req.params), 'vizzuality:my_table:png:point:select *:');
+};
+
+/**
+ * THE FOLLOWING TESTS NEED SOME DB SETUP
+ * They need a database setup as below with the table ine_poly defined
+ */
+
+tests['render_cache can generate a tilelive object'] = function(){
+    var req = {params: {dbname: "cartodb_user_123_db", table: 'ine_poly', x: 4, y:4, z:4, geom_type:'polygon', format:'png' }};
+
+    RenderCache.getRenderer(req, function(err, renderer){
+        assert.eql(renderer._uri.query.base, 'cartodb_user_123_db:ine_poly:png:polygon::');
+    });
+};
+
+
+tests['render_cache can generate > 1 tilelive object'] = function(){
+    var req = {params: {dbname: "cartodb_user_123_db", table: 'ine_poly', x: 4, y:4, z:4, geom_type:'polygon', format:'png' }};
+
+    RenderCache.getRenderer(req, function(err, renderer){
+        var req = {params: {dbname: "cartodb_user_123_db", table: 'gadm4', x: 4, y:4, z:4, geom_type:'polygon', format:'png' }};
+
+        RenderCache.getRenderer(req, function(err, renderer){
+            assert.eql(_.keys(RenderCache.renderers).length, 2);
+        });
+    });
+};
+
+
+tests['render_cache can reuses tilelive object'] = function(){
+    var req = {params: {dbname: "cartodb_user_123_db", table: 'ine_poly', x: 4, y:4, z:4, geom_type:'polygon', format:'png' }};
+
+    RenderCache.getRenderer(req, function(err, renderer){
+        RenderCache.getRenderer(req, function(err, renderer){
+            assert.eql(_.keys(RenderCache.renderers).length, 1);
+        });
+    });
+};
