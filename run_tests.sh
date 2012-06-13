418ca80a56234490f16da9723021c2df732adbc1
Sandro Santilli <strk@keybit.net>
2012-06-13 15:34:41 +0200

    Ensure service redis is shut down on exit or kill (#15)

--- run_tests.sh
+++ run_tests.sh
@@ -3,12 +3,31 @@
 # Must match module.exports.redis.port in config/environments/test.js
 REDIS_PORT=6333
 
+cleanup() {
+	echo "Cleaning up"
+	kill ${PID_REDIS}
+}
+
+cleanup_and_exit() {
+	cleanup
+	exit
+}
+
+die() {
+	msg=$1
+	echo "${msg}" >&2
+	cleanup
+	exit 1
+}
+
+trap 'cleanup_and_exit' 1 2 3 5 9 13
+
 echo "Starting redis on port ${REDIS_PORT}"
 echo "port ${REDIS_PORT}" | redis-server - > test.log &
 PID_REDIS=$!
 
 echo "Preparing the database"
-cd test; sh prepare_test >> test.log; cd -;
+cd test; sh prepare_test >> test.log || die "database preparation failure (see test.log)"; cd -;
 
 echo "Running unit tests"
 expresso test/unit/windshaft.test.js \
@@ -17,4 +36,4 @@ expresso test/unit/windshaft.test.js \
 # This one doesn't work for me (yet) --strk
 #expresso test/acceptance/server.js
 
-kill ${PID_REDIS}
+cleanup
