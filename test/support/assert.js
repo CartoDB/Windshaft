c1c5a161e4493734fe431a822bcd5bee982715cc
Sandro Santilli <strk@keybit.net>
2012-07-25 11:29:20 +0200

    Use a fuzzy tolerance of 2% color distance when comparing images

--- test/support/assert.js
+++ test/support/assert.js
@@ -15,27 +15,28 @@ assert.imageEqualsFile = function(buffer, file_b, callback) {
     var err = fs.writeFileSync(file_a, buffer, 'binary');
     if (err) throw err;
 
-    exec('compare -metric PSNR "' + file_a + '" "' +
+    // See http://www.imagemagick.org/script/command-line-options.php#metric
+    var fuzz = '2%';
+    exec('compare -fuzz ' + fuzz + ' -metric AE "' + file_a + '" "' +
             file_b + '" /dev/null', function(err, stdout, stderr) {
         if (err) {
             fs.unlinkSync(file_a);
             callback(err);
         } else {
             stderr = stderr.trim();
-            if (stderr === 'inf') {
-                fs.unlinkSync(file_a);
-                callback(null);
+            var similarity = parseFloat(stderr);
+            if ( similarity > 0 ) {
+              var err = new Error('Images not equal(' + similarity + '): ' +
+                      file_a  + '    ' + file_b);
+              err.similarity = similarity;
+              callback(err);
             } else {
-                var similarity = parseFloat(stderr);
-                var err = new Error('Images not equal(' + similarity + '): ' +
-                        file_a  + '    ' + file_b);
-                err.similarity = similarity;
-                callback(err);
+              fs.unlinkSync(file_a);
+              callback(null);
             }
         }
     });
 };
-
 
 /**
  * Assert response from `server` with
