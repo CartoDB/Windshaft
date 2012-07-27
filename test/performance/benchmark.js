090aa89b2c5b29caf564a05ca389044030513909
Sandro Santilli <strk@keybit.net>
2012-07-27 18:02:04 +0200

    Backport improvements to the benchmark tools

--- test/performance/benchmark.js
+++ test/performance/benchmark.js
@@ -1,12 +1,68 @@
+#!/usr/bin/env node
+
 // small benchmark to execute with nodejs
 
 var http = require('http')
+var me = process.argv[1];
+
+function usage(exit_code) {
+  console.log("Usage: " + me + " [OPTIONS] <baseurl>");
+  console.log("Options:");
+  console.log(" -v                verbose operations (off)");
+  console.log(" --key <string>    map authentication key (none)");
+  console.log(" --requests <num>  number of requests to send (1000)");
+  console.log(" --cached <num>    number of requests sharing same cache id (16)");
+  process.exit(exit_code);
+}
+
+process.argv.shift(); // this will be "node" (argv[0])
+process.argv.shift(); // this will be "benchmark.js" (argv[1])
+
+var verbose = 0;
+var baseurl;
+var map_key;
+var cached_requests = 16;
+var N = 1000;
+
+var arg;
+while ( arg = process.argv.shift() ) {
+  if ( arg == '-v' ) {
+    ++verbose;
+  }
+  else if ( arg == '--key' ) {
+    map_key=process.argv.shift();
+  }
+  else if ( arg == '--cached' ) {
+    cached_requests=process.argv.shift();
+  }
+  else if ( arg == '--requests' ) {
+    N = parseInt(process.argv.shift());
+  }
+  else if ( ! baseurl ) {
+    baseurl = arg;
+  }
+  else {
+    usage(1);
+  }
+}
+
+if ( ! baseurl ) {
+  usage(1);
+}
+
+console.log("Baseurl is " + baseurl);
+var baseurl_comps = baseurl.match(/(https?:\/\/)?([^:\/]*)(:([^\/]*))?(\/.*).*/);
 
 var options = {
-  host: 'vizzuality.localhost.lan',
-  port: 80,
-  path: '/tiles/datos_agroguia_2/{z}/{x}/{y}.png?cache_buster=0&map_key=a9edf3d0d2edbcf55ad38ee5b23af1507b774a5b'
+  host: baseurl_comps[2],
+  port: baseurl_comps[4] ? baseurl_comps[4] : 8181,
+  path: baseurl_comps[5] + '/{z}/{x}/{y}.png?cache_buster={cb}'
 };
+
+if ( map_key ) options.path += '&map_key=' + map_key;
+
+console.dir(options);
+console.log("Requests per cache: " + cached_requests);
 
 function randInt(min, max) {
     return min + Math.floor(Math.random()*(max- min +1));
@@ -20,29 +76,61 @@ function end() {
     console.log("error: ", error)
     console.log("time: ", t);
     console.log("req/s: ", ok/t);
+    process.exit(0);
 }
 
-var N = 1000
 var ok = 0;
 var error = 0;
+
+function fail(msg) {
+  console.log(msg);
+  ++ error;
+  if ( error + ok === N ) end();
+}
+
+function pass() {
+  ++ ok;
+  if ( error + ok === N ) end();
+}
+
 for(var i = 0; i < N; ++i) {
     var opt = {
         host: options.host,
         port: options.port,
         path: new String(options.path)
     };
-    opt.path = opt.path.replace('{z}', 2).replace('{x}', randInt(0, 3)).replace('{y}', randInt(0, 3));
-    console.log(opt.path)
+
+    var z = 2;
+    var x = randInt(0, 3);
+    var y = randInt(0, 3);
+    // update cache buster every 16 requests (TODO: use command line switch)
+    var cb = Math.floor(i/cached_requests);
+
+    opt.path = opt.path.replace('{z}', z).replace('{x}', x).replace('{y}', y).replace('{cb}', cb);
+    if ( verbose ) console.log(' http://' + opt.host + ':' + opt.port + opt.path);
+
+    //console.log(opt.path)
     http.get(opt, function(res) {
-        //console.log(ok + error);
-        ok++;
-        if(ok + error  === N) 
-            end();
+      res.body = '';
+      res.on('data', function(chunk) {
+        // Save only first chunk, to reduce cost of the operation
+        if ( res.body.length == 0 ) res.body += chunk;
+      });
+      res.on('end', function() {
+        if ( res.statusCode == 200 ) pass();
+        else {
+          fail(res.statusCode + ' http://' + opt.host + ':' + opt.port + opt.path + ' ' + res.body);
+          process.exit(1);
+        }
+      });
     }).on('error', function(e) {
-        //console.log(ok + error);
-        error ++;
-        if(ok + error  === N) 
-            end();
+        fail('unknown (http) error');
+        process.exit(1);
     });
 }
 
+var timeout = N * 1000;
+setTimeout(function() { 
+  console.log("Not finished after " + (timeout/1000) + " seconds, giving up");
+  process.exit(1);
+}, timeout);
