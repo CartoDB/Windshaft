93faec1debec4f3bb069e1fbde1208272098d416
Sandro Santilli <strk@keybit.net>
2012-08-01 18:41:37 +0200

    Backport improvements to the benchmarking script

--- test/performance/benchmark.js
+++ test/performance/benchmark.js
@@ -8,10 +8,12 @@ var me = process.argv[1];
 function usage(exit_code) {
   console.log("Usage: " + me + " [OPTIONS] <baseurl>");
   console.log("Options:");
-  console.log(" -v                verbose operations (off)");
-  console.log(" --key <string>    map authentication key (none)");
-  console.log(" --requests <num>  number of requests to send (1000)");
-  console.log(" --cached <num>    number of requests sharing same cache id (16)");
+  console.log(" -v                      verbose operations (off)");
+  console.log(" --help                  print this help");
+  console.log(" --key <string>          map authentication key (none)");
+  console.log(" -n, --requests <num>    number of requests to send (1000)");
+  console.log(" -C, --cached <num>      number of requests sharing same cache id (20)");
+  console.log(" -c, --concurrent <num>  number of concurrent requests (20)");
   process.exit(exit_code);
 }
 
@@ -21,8 +23,9 @@ process.argv.shift(); // this will be "benchmark.js" (argv[1])
 var verbose = 0;
 var baseurl;
 var map_key;
-var cached_requests = 16;
-var N = 1000;
+var cached_requests = 20;
+var N = cached_requests*50; // number of requests (50 full viewports)
+var concurrency = cached_requests; // number of concurrent requests
 
 var arg;
 while ( arg = process.argv.shift() ) {
@@ -32,11 +35,17 @@ while ( arg = process.argv.shift() ) {
   else if ( arg == '--key' ) {
     map_key=process.argv.shift();
   }
-  else if ( arg == '--cached' ) {
+  else if ( arg == '--cached' || arg == '-C' ) {
     cached_requests=process.argv.shift();
   }
-  else if ( arg == '--requests' ) {
+  else if ( arg == '--requests' || arg == '-n' ) {
     N = parseInt(process.argv.shift());
+  }
+  else if ( arg == '--help' ) {
+    usage(0);
+  }
+  else if ( arg == '-c' || arg == '--concurrency' ) {
+    concurrency = parseInt(process.argv.shift());
   }
   else if ( ! baseurl ) {
     baseurl = arg;
@@ -61,21 +70,27 @@ var options = {
 
 if ( map_key ) options.path += '&map_key=' + map_key;
 
-console.dir(options);
-console.log("Requests per cache: " + cached_requests);
-
 function randInt(min, max) {
     return min + Math.floor(Math.random()*(max- min +1));
 }
 
-var start_time = new Date().getTime();
+var start_time = Date.now();
 function end() {
-    var end_time = new Date().getTime();
+    var end_time = Date.now();
     var t = (end_time - start_time)/1000;
-    console.log("ok: ", ok)
-    console.log("error: ", error)
-    console.log("time: ", t);
-    console.log("req/s: ", ok/t);
+    console.log("");
+    console.log("Server Hostname:      ", options.host);
+    console.log("Server Port:          ", options.port);
+    console.log("");
+    console.log("Requests per cache:   ", cached_requests);
+    console.log("Base Path:            ", options.path);
+    console.log("");
+    console.log("Complete requests:    ", ok)
+    console.log("Failed requests:      ", error)
+    console.log("Concurrency Level:    ", concurrency);
+    console.log("Time taken for tests: ", t, " seconds");
+    console.log("Requests per second:: ", (Math.round(((ok+error)/t)*100)/100), '[#/sec] (mean)');
+    console.log("");
     process.exit(0);
 }
 
@@ -93,6 +108,7 @@ function pass() {
   if ( error + ok === N ) end();
 }
 
+http.globalAgent.maxSockets = concurrency;
 for(var i = 0; i < N; ++i) {
     var opt = {
         host: options.host,
@@ -100,10 +116,11 @@ for(var i = 0; i < N; ++i) {
         path: new String(options.path)
     };
 
-    var z = 2;
-    var x = randInt(0, 3);
-    var y = randInt(0, 3);
-    // update cache buster every 16 requests (TODO: use command line switch)
+    var z = 3;
+    var x = i%5; // TODO: make this configurable (5 horizontal tiles)
+    var y = i%4; // TODO: make this configurable (4 vertical tiles)
+
+    // update cache buster every "cached_requests" requests 
     var cb = Math.floor(i/cached_requests);
 
     opt.path = opt.path.replace('{z}', z).replace('{x}', x).replace('{y}', y).replace('{cb}', cb);
@@ -112,10 +129,12 @@ for(var i = 0; i < N; ++i) {
     //console.log(opt.path)
     http.get(opt, function(res) {
       res.body = '';
-      res.on('data', function(chunk) {
-        // Save only first chunk, to reduce cost of the operation
-        if ( res.body.length == 0 ) res.body += chunk;
-      });
+      if ( res.statusCode != 200 ) {
+        res.on('data', function(chunk) {
+          // Save only first chunk, to reduce cost of the operation
+          if ( res.body.length == 0 ) res.body += chunk;
+        });
+      }
       res.on('end', function() {
         if ( res.statusCode == 200 ) pass();
         else {
@@ -125,7 +144,6 @@ for(var i = 0; i < N; ++i) {
       });
     }).on('error', function(e) {
         fail('unknown (http) error');
-        process.exit(1);
     });
 }
 
