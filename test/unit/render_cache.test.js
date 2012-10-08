a8e6fde52e2964e2c3095c5c9ea4a5a088f4e60c
Sandro Santilli <strk@keybit.net>
2012-10-08 15:31:40 +0200

    Improve error handling

--- test/unit/render_cache.test.js
+++ test/unit/render_cache.test.js
@@ -39,6 +39,7 @@ suite('render_cache', function() {
 
         render_cache.getRenderer(req, function(err, renderer){
             assert.ok(renderer, err);
+            assert.ok(renderer.get(), err);
             assert.equal(renderer.get()._uri.query.base.split(':')[0], 'windshaft_test');
             done();
         });
