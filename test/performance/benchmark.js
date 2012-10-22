1131971e0211c842caaade3e9d3c0bf492060121
Sandro Santilli <strk@keybit.net>
2012-10-22 10:50:54 +0200

    Refactor benchmark app to fetch in chunks of full viewport tiles

--- test/performance/benchmark.js
+++ test/performance/benchmark.js
@@ -10,14 +10,15 @@ var me = process.argv[1];
 function usage(exit_code) {
   console.log("Usage: " + me + " [OPTIONS] <example_tile_url>");
   console.log("Options:");
-  console.log(" -v                      verbose operations (off)");
+  console.log(" -v                      increment verbosity by 1 (defaults to 0)");
   console.log(" --help                  print this help");
   console.log(" --key <string>          map authentication key (none)");
-  console.log(" -n, --requests <num>    number of requests to send (1000)");
-  console.log(" -C, --cached <num>      number of requests sharing same cache id (20)");
+  console.log(" -n, --requests <num>    Maximum number of requests to send (1000)");
+  console.log(" -C, --cached <num>      Viewport requests sharing same cache id (20)");
   console.log(" -c, --concurrent <num>  number of concurrent requests (20)");
-  console.log(" --vp-size <num>x<num>   tiles per col, line in viewport (5x4)");
-  console.log(" --zoom-levels <num>     number of zoom levels to span");
+  console.log(" -t, --timelimit <num>   Maximum number of seconds to spend for benchmarking (0)");
+  console.log(" --vp-size <num>x<num>   tiles per col, line in user viewport (5x4)");
+  console.log(" --zoom-levels <num>     number of zoom levels to span for each user");
   console.log(" --grid                  also fetch an utf8grid for each tile");
   process.exit(exit_code);
 }
@@ -35,6 +36,8 @@ var cols = 5;
 var lines = 4;
 var zlevs = 2; 
 var fetch_grid = false;
+var idletime = 0; // in seconds (TODO: parametrize)
+var timelimit = 0;
 
 var arg;
 while ( arg = process.argv.shift() ) {
@@ -46,6 +49,9 @@ while ( arg = process.argv.shift() ) {
   }
   else if ( arg == '--cached' || arg == '-C' ) {
     cached_requests=process.argv.shift();
+  }
+  else if ( arg == '--timelimit' || arg == '-t' ) {
+    timelimit = parseInt(process.argv.shift());
   }
   else if ( arg == '--grid' ) {
     fetch_grid=true;
@@ -109,19 +115,18 @@ function end() {
     console.log("Server Host:          ", urlparsed.host);
     console.log("Template URL (path):  ", urlparsed.pathname);
     console.log("");
-    console.log("Viewport size:        " + cols + "x" + lines);
-    console.log("Zoom levels:          " + zlevs);
+    console.log("Viewport size:        ", cols + "x" + lines);
+    console.log("Zoom levels:          ", zlevs);
+    console.log("Viewports per cache:  ", cached_requests);
+    console.log("Concurrency Level:    ", concurrency);
     console.log("");
-    console.log("Requests per cache:   ", cached_requests);
     console.log("Complete requests:    ", ok);
-    console.log("");
-    console.log("X-Cache hits:         ", xchits, " (" + Math.round((xchits/ok)*100) + "% of complete requests)" );
-    console.log("X-Varnish hits:       ", xvhits, " (" + Math.round((xvhits/ok)*100) + "% of complete requests)" );
-    console.log("Application hits:     ", ok-xchits-xvhits);
+    console.log("X-Cache hits:         ", xchits, " (" + Math.round((xchits/ok)*100) + "%)" );
+    console.log("X-Varnish hits:       ", xvhits, " (" + Math.round((xvhits/ok)*100) + "%)" );
+    console.log("Application hits:     ", ok-xchits-xvhits, " (" + Math.round(((ok-xchits-xvhits)/ok)*100) + "%)" );
     console.log("Hits summary:         ", xchits, "-", xvhits, "-", (ok-xchits-xvhits));
     console.log("");
     console.log("Failed requests:      ", error);
-    console.log("Concurrency Level:    ", concurrency);
     console.log("Time taken for tests: ", t, " seconds");
     console.log("Requests per second:  ", (Math.round(((ok+error)/t)*100)/100), '[#/sec] (mean)');
     console.log("");
@@ -133,83 +138,110 @@ var xchits = 0; // X-Cache hits
 var xvhits = 0; // X-Varnish hits
 var error = 0;
 
-function check_end() {
-  if ( error + ok === N ) end();
-}
-
-function fail(msg) {
-  console.log(msg);
-  ++ error;
-  check_end();
-}
-
-function pass() {
-  ++ ok;
-  check_end();
-}
-
 http.globalAgent.maxSockets = concurrency;
-var now = Date.now();
-var cbprefix = 'wb_' + process.env.USER + '_' + process.pid + "_"; 
-for(var i = 0; i < N; ++i) {
 
-    // we start at zoom level 3 (TODO: make configurable)
-    var z = 3 + Math.floor( (i/(cols*lines))%zlevs ); 
-    var x = i%cols;
-    var y = Math.floor(i/cols)%lines;
+if ( timelimit ) {
+  setTimeout(function() { 
+    console.log("Interrupting after " + timelimit + " seconds");
+    end();
+  }, timelimit*1000);
+}
 
-    // update cache_buster every "cached_requests" requests 
-    now += Math.floor(i/cached_requests);
-    var cb = cbprefix + now;
+var requests_per_viewport = cols * lines * ( fetch_grid ? 2 : 1 );
 
-    var nurlobj = url.parse(urltemplate, true);
-    delete nurlobj.search; // or url.format will not use urlparsed.query
-    nurlobj.pathname = nurlobj.pathname.replace('{z}', z).replace('{x}', x).replace('{y}', y);
-    if ( fetch_grid && i%2 ) {
-      nurlobj.pathname = nurlobj.pathname.replace('.png', '.grid.json');
-    }
-    nurlobj.query = nurlobj.query || {};
-    nurlobj.query['cache_buster'] = cb;
+function fetchViewport(x0, y0, z, cache_buster, callback)
+{
+  var im = fetch_grid ? 2 : 1;
+  var waiting = requests_per_viewport;
 
-    var nurl = url.format(nurlobj);
+  for (var xs=0; xs<cols; ++xs) {
+    var x = x0+xs;
+    for (var ys=0; ys<lines; ++ys) {
+      var y = y0+ys;
+      for (var i=0; i<im; ++i) {
 
-    if ( verbose ) console.log("|+++|---> " + nurl);
+        //console.log("Fetching " + z + "/" + x + "/" + y);
+        var nurlobj = url.parse(urltemplate, true);
+        delete nurlobj.search; // or url.format will not use urlparsed.query
+        nurlobj.pathname = nurlobj.pathname.replace('{z}', z).replace('{x}', x).replace('{y}', y);
+        if ( fetch_grid && i%2 ) {
+          nurlobj.pathname = nurlobj.pathname.replace('.png', '.grid.json');
+        }
+        nurlobj.query = nurlobj.query || {};
+        nurlobj.query['cache_buster'] = cache_buster;
 
-    //console.log(opt.path)
-    http.get(nurl, function(res) {
-      res.body = '';
-      if ( res.statusCode != 200 ) {
-        res.on('data', function(chunk) {
-          // Save only first chunk, to reduce cost of the operation
-          if ( res.body.length == 0 ) res.body += chunk;
+        var nurl = url.format(nurlobj);
+
+        if ( verbose > 1 ) console.log("|+++|---> " + nurl);
+
+        //console.log(opt.path)
+        http.get(nurl, function(res) {
+          res.body = '';
+          if ( res.statusCode != 200 ) {
+            res.on('data', function(chunk) {
+              // Save only first chunk, to reduce cost of the operation
+              if ( res.body.length == 0 ) res.body += chunk;
+            });
+          }
+          res.on('end', function() {
+            if ( res.statusCode == 200 ) {
+              var xcache = res.headers['x-cache'];
+              var xvarnish = res.headers['x-varnish'];
+              if ( xcache && xcache.match(/hit/i) ) ++xchits;
+              else {
+                if ( xvarnish && xvarnish.match(/ /) ) ++xvhits;
+              }
+              if ( verbose > 2 ) {
+                console.log("X-Cache: " + xcache);
+                console.log("X-Varnish: " + xvarnish);
+              }
+              ++ ok;
+              if ( ! --waiting ) callback();
+            }
+            else {
+              console.log(res.statusCode + ' ' + nurl + ' ' + res.body);
+              ++ error;
+              if ( ! --waiting ) callback();
+            }
+          });
+        }).on('error', function(e) {
+            console.log('unknown (http) error');
+            ++ error;
+            if ( ! --waiting ) callback();
         });
       }
-      res.on('end', function() {
-        if ( res.statusCode == 200 ) {
-          var xcache = res.headers['x-cache'];
-          var xvarnish = res.headers['x-varnish'];
-          if ( xcache && xcache.match(/hit/i) ) ++xchits;
-          else {
-            if ( xvarnish && xvarnish.match(/ /) ) ++xvhits;
-          }
-          if ( verbose ) {
-            console.log("X-Cache: " + xcache);
-            console.log("X-Varnish: " + xvarnish);
-          }
-          pass();
-        }
-        else {
-          fail(res.statusCode + ' ' + nurl + ' ' + res.body);
-          process.exit(1);
-        }
-      });
-    }).on('error', function(e) {
-        fail('unknown (http) error');
+    }
+  }
+}
+
+var now = Date.now();
+var cbprefix = 'wb_' + process.env.USER + '_' + process.pid + "_"; 
+var zstart = 3; // FIXME: make configurable
+var vpcount = 0;
+var users = 1;
+function fetchNextViewport() {
+
+    if ( vpcount * requests_per_viewport * users >= N ) end();
+    // TODO: use timeout as another exit point ?
+
+    // update cache_buster 
+    var cb = cbprefix + ( now + Math.floor(vpcount/cached_requests) );
+
+    // update zoom level 
+    var z = zstart + vpcount % zlevs;
+
+    // update vpcount
+    ++vpcount;
+
+    if ( verbose ) {
+      console.log("Fetching viewport " + vpcount + " at zoom level " + z + " with cache_buster " + cb);
+    }
+
+    fetchViewport(0, 0, z, cb, function() {
+      // fetch next viewport in idletime seconds
+      setTimeout(fetchNextViewport, idletime*1000);
     });
 }
 
-var timeout = N * 1000;
-setTimeout(function() { 
-  console.log("Not finished after " + (timeout/1000) + " seconds, giving up");
-  process.exit(1);
-}, timeout);
+
+fetchNextViewport();
