e69924a6d45e9de20e2328392eb5792955bed158
Sandro Santilli <strk@keybit.net>
2014-02-03 17:18:41 +0100

    Add test for fetching attributes with jsonp

--- test/acceptance/attributes.js
+++ test/acceptance/attributes.js
@@ -25,6 +25,23 @@ suite('attributes', function() {
     server.setMaxListeners(0);
     var redis_client = redis.createClient(ServerOptions.redis.port);
 
+    var test_mapconfig_1 =  {
+      version: '1.1.0',
+      layers: [
+         { type: 'mapnik', options: {
+             sql: "select 1 as id, 'SRID=4326;POINT(0 0)'::geometry as the_geom",
+             cartocss: '#style { }',
+             cartocss_version: '2.0.1'
+           } },
+         { type: 'mapnik', options: {
+             sql: "select 1 as i, 6 as n, 'SRID=4326;POINT(0 0)'::geometry as the_geom",
+             attributes: { id:'i', columns: ['n'] },
+             cartocss: '#style { }',
+             cartocss_version: '2.0.1'
+           } }
+      ]
+    };
+
     checkCORSHeaders = function(res) {
       var h = res.headers['access-control-allow-headers'];
       assert.ok(h);
@@ -48,22 +65,6 @@ suite('attributes', function() {
     test("can only be fetched from layer having an attributes spec",
     function(done) {
 
-      var mapconfig =  {
-        version: '1.1.0',
-        layers: [
-           { type: 'mapnik', options: {
-               sql: "select 1 as id, 'SRID=4326;POINT(0 0)'::geometry as the_geom",
-               cartocss: '#style { }',
-               cartocss_version: '2.0.1'
-             } },
-           { type: 'mapnik', options: {
-               sql: "select 1 as i, 6 as n, 'SRID=4326;POINT(0 0)'::geometry as the_geom",
-               attributes: { id:'i', columns: ['n'] },
-               cartocss: '#style { }',
-               cartocss_version: '2.0.1'
-             } }
-        ]
-      };
       var expected_token; 
       Step(
         function do_post()
@@ -73,7 +74,7 @@ suite('attributes', function() {
               url: '/database/windshaft_test/layergroup',
               method: 'POST',
               headers: {'Content-Type': 'application/json' },
-              data: JSON.stringify(mapconfig)
+              data: JSON.stringify(test_mapconfig_1)
           }, {}, function(res, err) { next(err, res); });
         },
         function checkPost(err, res) {
@@ -101,6 +102,80 @@ suite('attributes', function() {
           assert.equal(res.statusCode, 400, res.statusCode + ( res.statusCode != 200 ? (': ' + res.body) : '' ));
           var parsed = JSON.parse(res.body);
           assert.equal(parsed.error, "Layer 0 has no exposed attributes");
+          return null;
+        },
+        function do_get_attr_1(err)
+        {
+          if ( err ) throw err;
+          var next = this;
+          assert.response(server, {
+              url: '/database/windshaft_test/layergroup/' + expected_token + '/1/attributes/1',
+              method: 'GET'
+          }, {}, function(res, err) { next(err, res); });
+        },
+        function check_attr_1(err, res) {
+          if ( err ) throw err;
+          assert.equal(res.statusCode, 200, res.statusCode + ': ' + res.body);
+          var parsed = JSON.parse(res.body);
+          assert.deepEqual(parsed, {"n":6});
+          return null;
+        },
+        function finish(err) {
+          var errors = [];
+          if ( err ) errors.push(''+err);
+          redis_client.exists("map_cfg|" +  expected_token, function(err, exists) {
+              if ( err ) errors.push(err.message);
+              //assert.ok(exists, "Missing expected token " + expected_token + " from redis");
+              redis_client.del("map_cfg|" +  expected_token, function(err) {
+                if ( err ) errors.push(err.message);
+                if ( errors.length ) done(new Error(errors));
+                else done(null);
+              });
+          });
+        }
+      );
+    });
+
+    test("can be used with jsonp", function(done) {
+
+      var expected_token; 
+      Step(
+        function do_post()
+        {
+          var next = this;
+          assert.response(server, {
+              url: '/database/windshaft_test/layergroup',
+              method: 'POST',
+              headers: {'Content-Type': 'application/json' },
+              data: JSON.stringify(test_mapconfig_1)
+          }, {}, function(res, err) { next(err, res); });
+        },
+        function checkPost(err, res) {
+          if ( err ) throw err;
+          assert.equal(res.statusCode, 200, res.statusCode + ': ' + res.body);
+          // CORS headers should be sent with response
+          // from layergroup creation via POST
+          checkCORSHeaders(res);
+          var parsedBody = JSON.parse(res.body);
+          if ( expected_token ) assert.deepEqual(parsedBody, {layergroupid: expected_token, layercount: 2});
+          else expected_token = parsedBody.layergroupid;
+          return null;
+        },
+        function do_get_attr_0(err)
+        {
+          if ( err ) throw err;
+          var next = this;
+          assert.response(server, {
+              url: '/database/windshaft_test/layergroup/' + expected_token +
+                   '/0/attributes/1?callback=test',
+              method: 'GET'
+          }, {}, function(res, err) { next(err, res); });
+        },
+        function check_error_0(err, res) {
+          if ( err ) throw err;
+          // jsonp errors should be returned with HTTP status 200
+          assert.equal(res.statusCode, 200, res.statusCode + ': ' + res.body);
+          assert.equal(res.body, 'test({"error":"Layer 0 has no exposed attributes"});');
           return null;
         },
         function do_get_attr_1(err)
