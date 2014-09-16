ab7dcbb8d965c7e44feec442d830c655cd28cb30
Raul Ochoa <rochoaf@gmail.com>
2014-09-16 17:02:48 +0200

    Modifies strk/pgraster to support gcols to use objects for configuring type/name for the geom/raster column

--- config/environments/test.js
+++ config/environments/test.js
@@ -7,6 +7,6 @@ module.exports.redis            = {host: '127.0.0.1',
                                    port: 6334, // 6379 is the default, 6333 is used by grainstore
                                    idleTimeoutMillis: 1,
                                    reapIntervalMillis: 1};
-module.exports.mapnik_version   = undefined; // will be looked up at runtime if undefined
+module.exports.mapnik_version   = '2.2.0'; // will be looked up at runtime if undefined
 module.exports.windshaft_port   = 8083;
 module.exports.enable_cors      = true;
