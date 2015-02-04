9ea5a8ca4ae0df2be500c58b676528d110e6131c
Raul Ochoa <rochoaf@gmail.com>
2015-02-04 11:41:04 +0100

    Improve isEmpty method by filtering non-undefined items

--- test/unit/datasource.test.js
+++ test/unit/datasource.test.js
@@ -9,6 +9,21 @@ suite('datasource', function() {
         var emptyDatasource = Datasource.EmptyDatasource();
         assert.ok(emptyDatasource.isEmpty());
         done();
-    })
+    });
 
+    test('all layers as undefined should report an empty datasource', function(done) {
+        var datasourceBuilder = new Datasource.Builder();
+        datasourceBuilder.withLayerDatasource(0, undefined);
+        var emptyDatasource = datasourceBuilder.build();
+        assert.ok(emptyDatasource.isEmpty());
+        done();
+    });
+
+    test('at least one non-undefined should NOT report an empty datasource', function(done) {
+        var datasourceBuilder = new Datasource.Builder();
+        datasourceBuilder.withLayerDatasource(0, {user: 'foo'});
+        var emptyDatasource = datasourceBuilder.build();
+        assert.equal(emptyDatasource.isEmpty(), false);
+        done();
+    });
 });
