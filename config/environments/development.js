755bf11eb13661234e605b0e6e98125ba0ca2366
Raul Ochoa <rochoaf@gmail.com>
2015-02-21 21:04:49 +0100

    Format config files

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,10 +1,18 @@
-module.exports.name             = 'development';
-module.exports.postgres         = {user: 'postgres', host: '127.0.0.1', port: 5432, geometry_field: 'the_geom', srid: 4326 };
-module.exports.millstone        = {cache_basedir: '/tmp/windshaft-dev/millstone'};
-module.exports.redis            = {host: '127.0.0.1', 
-                                   port: 6379, 
-                                   idleTimeoutMillis: 1,
-                                   reapIntervalMillis: 1};
-module.exports.mapnik_version   = undefined; // will be looked up at runtime if undefined
-module.exports.windshaft_port   = 8080;
-module.exports.enable_cors      = true;
+module.exports.name = 'development';
+module.exports.postgres = {
+    user: 'postgres',
+    host: '127.0.0.1',
+    port: 5432,
+    geometry_field: 'the_geom',
+    srid: 4326
+};
+module.exports.millstone = {cache_basedir: '/tmp/windshaft-dev/millstone'};
+module.exports.redis = {
+    host: '127.0.0.1',
+    port: 6379,
+    idleTimeoutMillis: 1,
+    reapIntervalMillis: 1
+};
+module.exports.mapnik_version = undefined; // will be looked up at runtime if undefined
+module.exports.windshaft_port = 8080;
+module.exports.enable_cors = true;
--- config/environments/test.js
+++ config/environments/test.js
@@ -1,13 +1,20 @@
-module.exports.name             = 'test';
+module.exports.name = 'test';
 // Allowed elements in "postgres" config object:
 // user, host, port, geometry_field, srid
-module.exports.postgres         = {geometry_field: 'the_geom', srid: 4326};
-module.exports.millstone        = {cache_basedir: '/tmp/windshaft-test/millstone'};
-module.exports.redis            = {host: '127.0.0.1', 
-                                   port: 6334, // 6379 is the default, 6333 is used by grainstore
-                                   idleTimeoutMillis: 1,
-                                   reapIntervalMillis: 1};
-module.exports.renderer         = {};
-module.exports.mapnik_version   = undefined; // will be looked up at runtime if undefined
-module.exports.windshaft_port   = 8083;
-module.exports.enable_cors      = true;
+module.exports.postgres = {
+    geometry_field: 'the_geom',
+    srid: 4326
+};
+module.exports.millstone = {
+    cache_basedir: '/tmp/windshaft-test/millstone'
+};
+module.exports.redis = {
+    host: '127.0.0.1',
+    port: 6334, // 6379 is the default, 6333 is used by grainstore
+    idleTimeoutMillis: 1,
+    reapIntervalMillis: 1
+};
+module.exports.renderer = {};
+module.exports.mapnik_version = undefined; // will be looked up at runtime if undefined
+module.exports.windshaft_port = 8083;
+module.exports.enable_cors = true;
