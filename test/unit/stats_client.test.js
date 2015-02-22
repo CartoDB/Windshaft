d5c7821ffdd2e877beb3b3f2cf3a7fbac407a26c
Raul Ochoa <rochoaf@gmail.com>
2015-02-22 02:16:43 +0100

    Adds test to validate stats client report same error when it repeats

---
+++ test/unit/stats_client.test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var StatsClient = require('../../lib/windshaft/stats/client');
+
+suite('stats client', function() {
+    var statsInstance;
+
+    suiteSetup(function() {
+        statsInstance = StatsClient.instance;
+        StatsClient.instance = null;
+    });
+
+    test('reports errors when they repeat', function(done) {
+        var statsClient = StatsClient.getInstance({ host: '127.0.0.1', port: 8033 });
+        statsClient.increment('foo');
+        console.log(statsClient.socket.emit('error', 'wadus_error'));
+        console.log(statsClient.socket.emit('error', 'wadus_error'));
+        setTimeout(done, 2000);
+    });
+
+    suiteTeardown(function() {
+        StatsClient.instance = statsInstance;
+    });
+});
