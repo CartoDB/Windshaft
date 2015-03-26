01cd5d09613e9d1333a862e80ad56cda41bda923
Raul Ochoa <rochoaf@gmail.com>
2015-03-26 19:29:37 +0100

    Adds a global afterEach hook to validate no redis keys

--- test/support/test_helper.js
+++ test/support/test_helper.js
@@ -6,9 +6,25 @@
  */
 
 var _ = require('underscore');
+var assert = require('assert');
+var redis = require('redis');
 
 // set environment specific variables
 global.settings     = require(__dirname + '/../../config/settings');
 global.environment  = require(__dirname + '/../../config/environments/test');
 _.extend(global.settings, global.environment);
 process.env.NODE_ENV = 'test';
+
+
+var redisClient = redis.createClient(global.environment.redis.port);
+
+afterEach(function(done) {
+    // Check that we start with an empty redis db
+    redisClient.keys("*", function(err, matches) {
+        if ( err ) {
+            return done(err);
+        }
+        assert.equal(matches.length, 0, "redis keys present at setup time:\n" + matches.join("\n"));
+        redisClient.flushall(done);
+    });
+});
