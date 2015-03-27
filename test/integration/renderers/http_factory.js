8d7a2ea2a205522634df63c93d79d5ca9dc24929
Raul Ochoa <rochoaf@gmail.com>
2015-03-27 11:50:59 +0100

    Add test_helper in integration and move to describe/it

--- test/integration/renderers/http_factory.js
+++ test/integration/renderers/http_factory.js
@@ -1,9 +1,11 @@
+require('../../support/test_helper.js');
+
 var assert = require('assert');
 var HttpRendererFactory = require('../../../lib/windshaft/renderers/http/factory');
 var HttpFallbackRenderer = require('../../../lib/windshaft/renderers/http/fallback_renderer');
 var MapConfig = require('../../../lib/windshaft/models/mapconfig');
 
-suite('renderer_http_factory_getRenderer', function() {
+describe('renderer_http_factory_getRenderer', function() {
 
     var whitelistSample = [
         'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
@@ -14,7 +16,7 @@ suite('renderer_http_factory_getRenderer', function() {
 
     var factory = new HttpRendererFactory(whitelistSample, 2000);
 
-    test('getRenderer throws error for empty urlTemplate option', function(done) {
+    it('getRenderer throws error for empty urlTemplate option', function(done) {
         var factory = new HttpRendererFactory(whitelistSample, 2000);
         var mapConfig = MapConfig.create({
             layers: [
@@ -34,7 +36,7 @@ suite('renderer_http_factory_getRenderer', function() {
         });
     });
 
-    test('getRenderer throws error for invalid urlTemplate', function(done) {
+    it('getRenderer throws error for invalid urlTemplate', function(done) {
         var mapConfig = MapConfig.create({
             layers: [
                 {
@@ -54,7 +56,7 @@ suite('renderer_http_factory_getRenderer', function() {
         });
     });
 
-    test('getRenderer returns a fallback image renderer for invalid urlTemplate', function(done) {
+    it('getRenderer returns a fallback image renderer for invalid urlTemplate', function(done) {
         var factoryWithFallbackImage = new HttpRendererFactory(
             whitelistSample, 2000, undefined, 'http://example.com/fallback.png'
         );
