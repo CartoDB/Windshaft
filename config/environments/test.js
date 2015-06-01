fef7bf1b6775b8d6a019b547e2c1b63caa13374c
Brian Hare <bhare1985@gmail.com>
2015-06-01 01:05:34 -0400

    Change to newest mapnik/tilelive and windows compatiable and newest grainstore. Implemented new grainstore syntax. Tests not yet converted

--- config/environments/test.js
+++ config/environments/test.js
@@ -1,5 +1,5 @@
 module.exports.name             = 'test';
-module.exports.postgres               = {user: 'postgres', host: '127.0.0.1', port: 5432};
+module.exports.postgres         = {user: 'postgres', password: '708050', host: '127.0.0.1', port: 5432};
 module.exports.redis            = {host: '127.0.0.1', 
                                    port: 6379, 
                                    idleTimeoutMillis: 1,
