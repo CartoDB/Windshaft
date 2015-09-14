7fe4d758de8c1cd55f3abf26967326f1f1a867ff
Raul Ochoa <rochoaf@gmail.com>
2015-09-14 17:49:38 +0200

    Do not use setTimeout in test

--- test/unit/stats_client.test.js
+++ test/unit/stats_client.test.js
@@ -1,5 +1,7 @@
 require('../support/test_helper');
 
+var assert = require('assert');
+
 var StatsClient = require('../../lib/windshaft/stats/client');
 
 describe('stats client', function() {
@@ -10,15 +12,29 @@ describe('stats client', function() {
         StatsClient.instance = null;
     });
 
-    it('reports errors when they repeat', function(done) {
-        var statsClient = StatsClient.getInstance({ host: '127.0.0.1', port: 8033 });
-        statsClient.increment('foo');
-        statsClient.socket.emit('error', 'wadus_error');
-        statsClient.socket.emit('error', 'wadus_error');
-        setTimeout(done, 2000);
-    });
-
     after(function() {
         StatsClient.instance = statsInstance;
+    });
+
+    it('reports errors when they repeat', function(done) {
+        var WADUS_ERROR = 'wadus_error';
+        var statsClient = StatsClient.getInstance({ host: '127.0.0.1', port: 8033 });
+
+        statsClient.socket.emit('error', 'other_error');
+        assert.ok(statsClient.last_error);
+        assert.equal(statsClient.last_error.msg, 'other_error');
+        assert.ok(!statsClient.last_error.interval);
+
+        statsClient.socket.emit('error', WADUS_ERROR);
+        assert.ok(statsClient.last_error);
+        assert.equal(statsClient.last_error.msg, WADUS_ERROR);
+        assert.ok(!statsClient.last_error.interval);
+
+        statsClient.socket.emit('error', WADUS_ERROR);
+        assert.ok(statsClient.last_error);
+        assert.equal(statsClient.last_error.msg, WADUS_ERROR);
+        assert.ok(statsClient.last_error.interval);
+
+        done();
     });
 });
