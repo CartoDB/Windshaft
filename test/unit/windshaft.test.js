3ebc21591418d67bc88c0a677ff1a33e8d3736c0
Raul Ochoa <rochoaf@gmail.com>
2015-09-14 17:32:41 +0200

    Remove windshaft server tests

--- test/unit/windshaft.test.js
+++ test/unit/windshaft.test.js
@@ -1,58 +1,12 @@
 require('../support/test_helper.js');
 
-var _ = require('underscore');
 var assert = require('assert');
 var windshaft = require('../../lib/windshaft');
-var serverOptions = require('../support/server_options');
 
 describe('windshaft', function() {
 
     it('should have valid global environment',  function() {
         assert.equal(global.environment.name, 'test');
-    });
-
-    it('can instantiate a Windshaft object (configured express instance)', function(){
-        var ws = new windshaft.Server(serverOptions);
-        assert.ok(ws);
-    });
-
-    it('can spawn a new server on the global listen port', function(done){
-        var ws = new windshaft.Server(serverOptions);
-        ws.listen(global.environment.windshaft_port, function() {
-          assert.ok(ws);
-          ws.close(done); /* allow proper tear down */
-        });
-    });
-
-    it('throws exception if incorrect options passed in', function(){
-        assert.throws(
-            function(){
-                var ws = new windshaft.Server({unbuffered_logging:true});
-                ws.listen();
-            }, /Must initialise Windshaft with: 'base_url'\/'base_url_mapconfig' URLs and req2params function/
-        );
-    });
-
-    it('options are set on main windshaft object',  function(){
-        var ws = new windshaft.Server(serverOptions);
-        assert.ok(_.isFunction(ws.req2params));
-        assert.equal(ws.base_url, '/database/:dbname/table/:table');
-    });
-
-    it('different formats for postgis plugin error returns 400 as status code', function() {
-        var ws = new windshaft.Server(serverOptions);
-        var expectedStatusCode = 400;
-        assert.equal(
-            ws.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for single line does not match"
-        );
-
-        assert.equal(
-            ws.findStatusCode("Postgis Plugin: PSQL error:\nERROR:  column \"missing\" does not exist\n"),
-            expectedStatusCode,
-            "Error status code for multiline/PSQL does not match"
-        );
     });
 
     it('should expose version numbers', function() {
