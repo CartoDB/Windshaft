6684b7d91a4b68eaef4b45d6b1e43111302f5c02
Raul Ochoa <rochoaf@gmail.com>
2015-09-14 15:48:03 +0200

    rewrite windshaft server tests

--- test/acceptance/server.js
+++ test/acceptance/server.js
@@ -1,122 +1,23 @@
 require('../support/test_helper');
 
 var assert = require('../support/assert');
-var fs = require('fs');
-var Windshaft = require('../../lib/windshaft');
-var ServerOptions = require('../support/server_options');
-var http = require('http');
-var testClient = require('../support/test_client_old');
-
-function rmdir_recursive_sync(dirname) {
-  var files = fs.readdirSync(dirname);
-  for (var i=0; i<files.length; ++i) {
-    var f = dirname + "/" + files[i];
-    var s = fs.lstatSync(f);
-    if ( s.isFile() ) {
-      fs.unlinkSync(f);
-    } else {
-        rmdir_recursive_sync(f);
-    }
-  }
-}
-
-describe('server', function() {
-
-    var server = new Windshaft.Server(ServerOptions);
-    server.setMaxListeners(0);
-    var res_serv; // resources server
-    var res_serv_status = { numrequests:0 }; // status of resources server
-    var res_serv_port = 8033; // FIXME: make configurable ?
-
-    before(function(done) {
-
-        // Start a server to test external resources
-        res_serv = http.createServer( function(request, response) {
-            ++res_serv_status.numrequests;
-            var filename = __dirname + '/../fixtures/markers' + request.url;
-            fs.readFile(filename, "binary", function(err, file) {
-              if ( err ) {
-                response.writeHead(404, {'Content-Type': 'text/plain'});
-                response.write("404 Not Found\n");
-              } else {
-                response.writeHead(200);
-                response.write(file, "binary");
-              }
-              response.end();
-            });
-        });
-        res_serv.listen(res_serv_port, done);
-
-    });
-
-    after(function(done) {
-        rmdir_recursive_sync(global.environment.millstone.cache_basedir);
-
-        // Close the resources server
-        res_serv.close(done);
-    });
+var TestClient = require('../support/test_client');
 
 
-    ////////////////////////////////////////////////////////////////////
-    //
-    // GET INVALID
-    //
-    ////////////////////////////////////////////////////////////////////
+describe('basic grid json', function() {
 
-    it("get call to server returns 200",  function(done){
-        assert.response(server, {
-            url: '/',
-            method: 'GET'
-        },{
-            // FIXME: shouldn't this be a 404 ?
-            status: 200
-        }, function() { done(); } );
-    });
-
-    ////////////////////////////////////////////////////////////////////
-    //
-    // GET VERSION
-    //
-    ////////////////////////////////////////////////////////////////////
-
-    it("get /version returns versions",  function(done){
-        assert.response(server, {
-            url: '/version',
-            method: 'GET'
-        },{
-            status: 200
-        }, function(res) {
-          var parsed = JSON.parse(res.body);
-          assert.ok(parsed.hasOwnProperty('windshaft'), "No 'windshaft' version in " + parsed);
-          assert.ok(parsed.hasOwnProperty('grainstore'), "No 'grainstore' version in " + parsed);
-          assert.ok(parsed.hasOwnProperty('node_mapnik'), "No 'node_mapnik' version in " + parsed);
-          assert.ok(parsed.hasOwnProperty('mapnik'), "No 'mapnik' version in " + parsed);
-          // TODO: check actual versions ?
-          done();
-        });
-    });
-
-    ////////////////////////////////////////////////////////////////////
-    //
-    // GET GRID
-    //
-    ////////////////////////////////////////////////////////////////////
-
-    it("grid jsonp",  function(done){
-        var mapConfig = testClient.singleLayerMapConfig('select * from test_table', null, null, 'name');
-        testClient.getGridJsonp(mapConfig, 0, 13, 4011, 3088, 'test', function(err, res) {
-            assert.equal(res.statusCode, 200, res.body);
-            assert.deepEqual(res.headers['content-type'], 'application/json; charset=utf-8');
-            var regexp = '^test\\((.*)\\);$';
-            var matches = res.body.match(regexp);
-            assert.equal(matches.length, 2, 'Unexpected JSONP response:'  + res.body);
-            assert.utfgridEqualsFile(matches[1], './test/fixtures/test_table_13_4011_3088.grid.json', 2, done);
+    it("get grid",  function(done){
+        var mapConfig = TestClient.singleLayerMapConfig('select * from test_table', null, null, 'name');
+        var testClient = new TestClient(mapConfig);
+        testClient.getTile(13, 4011, 3088, { layer: 0, format: 'grid.json'}, function(err, tile) {
+            assert.utfgridEqualsFile(tile, './test/fixtures/test_table_13_4011_3088.grid.json', 2, done);
         });
     });
 
     it("get'ing a json with default style and single interactivity should return a grid",  function(done){
-        var mapConfig = testClient.singleLayerMapConfig('select * from test_table', null, null, 'name');
-        testClient.getGrid(mapConfig, 0, 13, 4011, 3088, function(err, res) {
+        var mapConfig = TestClient.singleLayerMapConfig('select * from test_table', null, null, 'name');
+        var testClient = new TestClient(mapConfig);
+        testClient.getTile(13, 4011, 3088, { layer: 0, format: 'grid.json'}, function(err, tile) {
             var expected_json = {
                 "1":{"name":"Hawai"},
                 "2":{"name":"El Estocolmo"},
@@ -124,35 +25,17 @@ describe('server', function() {
                 "4":{"name":"El Lacón"},
                 "5":{"name":"El Pico"}
             };
-            assert.deepEqual(JSON.parse(res.body).data, expected_json);
+            assert.deepEqual(tile.data, expected_json);
             done();
         });
     });
 
     it("get'ing a json with default style and no interactivity should return an error",  function(done){
-        var mapConfig = testClient.singleLayerMapConfig('select * from test_table');
-        var expectedResponse = {
-            status: 400,
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            }
-        };
-        testClient.getGrid(mapConfig, 0, 13, 4011, 3088, expectedResponse, function(err, res) {
-            assert.deepEqual(JSON.parse(res.body), { errors: ["Tileset has no interactivity"] });
-            done();
-        });
-    });
-
-    it("get grid jsonp error is returned with 200 status",  function(done){
-        var mapConfig = testClient.singleLayerMapConfig('select * from test_table');
-        var expectedResponse = {
-            status: 200,
-            headers: {
-                'Content-Type': 'text/javascript; charset=utf-8'
-            }
-        };
-        testClient.getGridJsonp(mapConfig, 0, 13, 4011, 3088, 'test', expectedResponse, function(err, res) {
-            assert.ok(res.body.match(/"errors":/), 'missing error in response: ' + res.body);
+        var mapConfig = TestClient.singleLayerMapConfig('select * from test_table');
+        var testClient = new TestClient(mapConfig);
+        testClient.getTile(13, 4011, 3088, { layer: 0, format: 'grid.json'}, function(err) {
+            assert.ok(err);
+            assert.equal(err.message, "Tileset has no interactivity");
             done();
         });
     });
@@ -160,18 +43,20 @@ describe('server', function() {
     // See http://github.com/Vizzuality/Windshaft/issues/50
     it("get'ing a json with no data should return an empty grid",  function(done){
         var query = 'select * from test_table limit 0';
-        var mapConfig = testClient.singleLayerMapConfig(query, null, null, 'name');
-        testClient.getGrid(mapConfig, 0, 13, 4011, 3088, function(err, res) {
-            assert.utfgridEqualsFile(res.body, './test/fixtures/test_table_13_4011_3088_empty.grid.json', 2, done);
+        var mapConfig = TestClient.singleLayerMapConfig(query, null, null, 'name');
+        var testClient = new TestClient(mapConfig);
+        testClient.getTile(13, 4011, 3088, { layer: 0, format: 'grid.json'}, function(err, tile) {
+            assert.utfgridEqualsFile(tile, './test/fixtures/test_table_13_4011_3088_empty.grid.json', 2, done);
         });
     });
 
     // Another test for http://github.com/Vizzuality/Windshaft/issues/50
     it("get'ing a json with no data but interactivity should return an empty grid",  function(done){
         var query = 'SELECT * FROM test_table limit 0';
-        var mapConfig = testClient.singleLayerMapConfig(query, null, null, 'cartodb_id');
-        testClient.getGrid(mapConfig, 0, 13, 4011, 3088, function(err, res) {
-            assert.utfgridEqualsFile(res.body, './test/fixtures/test_table_13_4011_3088_empty.grid.json', 2, done);
+        var mapConfig = TestClient.singleLayerMapConfig(query, null, null, 'cartodb_id');
+        var testClient = new TestClient(mapConfig);
+        testClient.getTile(13, 4011, 3088, { layer: 0, format: 'grid.json'}, function(err, tile) {
+            assert.utfgridEqualsFile(tile, './test/fixtures/test_table_13_4011_3088_empty.grid.json', 2, done);
         });
     });
 
@@ -179,15 +64,17 @@ describe('server', function() {
     it("get'ing a solid grid while changing interactivity fields",  function(done){
         var query = 'SELECT * FROM test_big_poly';
         var style211 = "#test_big_poly{polygon-fill:blue;}"; // for solid
-        var mapConfigName = testClient.singleLayerMapConfig(query, style211, null, 'name');
-        testClient.getGrid(mapConfigName, 0, 3, 2, 2, function(err, res) {
+        var mapConfigName = TestClient.singleLayerMapConfig(query, style211, null, 'name');
+        var testClient = new TestClient(mapConfigName);
+        testClient.getTile(3, 2, 2, { layer: 0, format: 'grid.json'}, function(err, tile) {
             var expected_data = { "1":{"name":"west"} };
-            assert.deepEqual(JSON.parse(res.body).data, expected_data);
+            assert.deepEqual(tile.data, expected_data);
 
-            var mapConfigCartodbId = testClient.singleLayerMapConfig(query, style211, null, 'cartodb_id');
-            testClient.getGrid(mapConfigCartodbId, 0, 3, 2, 2, function(err, res) {
+            var mapConfigCartodbId = TestClient.singleLayerMapConfig(query, style211, null, 'cartodb_id');
+            testClient = new TestClient(mapConfigCartodbId);
+            testClient.getTile(3, 2, 2, { layer: 0, format: 'grid.json'}, function(err, tile) {
                 var expected_data = { "1":{"cartodb_id":"1"} };
-                assert.deepEqual(JSON.parse(res.body).data, expected_data);
+                assert.deepEqual(tile.data, expected_data);
                 done();
             });
         });
--- test/unit/windshaft.test.js
+++ test/unit/windshaft.test.js
@@ -2,7 +2,7 @@ require('../support/test_helper.js');
 
 var _ = require('underscore');
 var assert = require('assert');
-var Windshaft = require('../../lib/windshaft');
+var windshaft = require('../../lib/windshaft');
 var serverOptions = require('../support/server_options');
 
 describe('windshaft', function() {
@@ -12,12 +12,12 @@ describe('windshaft', function() {
     });
 
     it('can instantiate a Windshaft object (configured express instance)', function(){
-        var ws = new Windshaft.Server(serverOptions);
+        var ws = new windshaft.Server(serverOptions);
         assert.ok(ws);
     });
 
     it('can spawn a new server on the global listen port', function(done){
-        var ws = new Windshaft.Server(serverOptions);
+        var ws = new windshaft.Server(serverOptions);
         ws.listen(global.environment.windshaft_port, function() {
           assert.ok(ws);
           ws.close(done); /* allow proper tear down */
@@ -27,20 +27,20 @@ describe('windshaft', function() {
     it('throws exception if incorrect options passed in', function(){
         assert.throws(
             function(){
-                var ws = new Windshaft.Server({unbuffered_logging:true});
+                var ws = new windshaft.Server({unbuffered_logging:true});
                 ws.listen();
             }, /Must initialise Windshaft with: 'base_url'\/'base_url_mapconfig' URLs and req2params function/
         );
     });
 
     it('options are set on main windshaft object',  function(){
-        var ws = new Windshaft.Server(serverOptions);
+        var ws = new windshaft.Server(serverOptions);
         assert.ok(_.isFunction(ws.req2params));
         assert.equal(ws.base_url, '/database/:dbname/table/:table');
     });
 
     it('different formats for postgis plugin error returns 400 as status code', function() {
-        var ws = new Windshaft.Server(serverOptions);
+        var ws = new windshaft.Server(serverOptions);
         var expectedStatusCode = 400;
         assert.equal(
             ws.findStatusCode("Postgis Plugin: ERROR:  column \"missing\" does not exist\n"),
@@ -53,6 +53,16 @@ describe('windshaft', function() {
             expectedStatusCode,
             "Error status code for multiline/PSQL does not match"
         );
+    });
+
+    it('should expose version numbers', function() {
+        assert.ok(windshaft.hasOwnProperty('version'), "No 'windshaft' version in " + windshaft);
+
+        var versions = windshaft.versions;
+        assert.ok(versions.hasOwnProperty('windshaft'), "No 'windshaft' version in " + versions);
+        assert.ok(versions.hasOwnProperty('grainstore'), "No 'grainstore' version in " + versions);
+        assert.ok(versions.hasOwnProperty('node_mapnik'), "No 'node_mapnik' version in " + versions);
+        assert.ok(versions.hasOwnProperty('mapnik'), "No 'mapnik' version in " + versions);
     });
 
 });
