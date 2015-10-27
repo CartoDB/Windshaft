cfd73cdd6e576d71f13f53262f5b6e42bed4de50
Raul Ochoa <rochoaf@gmail.com>
2015-10-27 17:32:43 +0100

    Support accept and reject in category filter

--- test/unit/mapconfig_filters.test.js
+++ test/unit/mapconfig_filters.test.js
@@ -5,7 +5,7 @@ var MapConfig = require('../../lib/windshaft/models/mapconfig');
 
 describe('mapconfig filters', function() {
 
-    it('should return an object with lists from config', function() {
+    describe('should return an object with filtered aggregations', function() {
         var listsMapConfig = {
             version: '1.5.0',
             layers: [
@@ -28,29 +28,86 @@ describe('mapconfig filters', function() {
                 }
             ]
         };
-        var mapConfig = MapConfig.create(listsMapConfig);
-        var filteredMapConfig = mapConfig.applyFilters({layers: [{
-            adm0name: { // this is category filter associated to the aggregation widget
-                accept: ['Spain']
-            }
-        }]});
 
-        var filteredList = filteredMapConfig.getWidget(0, 'adm0name');
-        assert.equal(filteredList.sql(),
-                "SELECT count(*) AS count, adm0name FROM" +
-                " (SELECT * FROM" +
+        it('uses accept filter param', function() {
+            var mapConfig = MapConfig.create(listsMapConfig);
+            var acceptFilterMapConfig = mapConfig.applyFilters({layers: [{
+                adm0name: { // this is category filter associated to the aggregation widget
+                    accept: ['Spain']
+                }
+            }]});
+
+            var filteredList = acceptFilterMapConfig.getWidget(0, 'adm0name');
+            assert.equal(filteredList.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (SELECT * FROM" +
                     " (select * from populated_places_simple_reduced) " +
                     "_cdb_category_filter WHERE adm0name IN ('Spain')) " +
-                "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
-        );
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
 
+            // check original mapconfig keeps it right
+            var list = mapConfig.getWidget(0, 'adm0name');
+            assert.equal(list.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (select * from populated_places_simple_reduced) " +
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
+        });
 
-        var list = mapConfig.getWidget(0, 'adm0name');
-        assert.equal(list.sql(),
-                "SELECT count(*) AS count, adm0name FROM" +
-                " (select * from populated_places_simple_reduced) " +
-                "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
-        );
+        it('uses reject filter param', function() {
+            var mapConfig = MapConfig.create(listsMapConfig);
+
+            var rejectFilterMapConfig = mapConfig.applyFilters({layers: [{
+                adm0name: { // this is category filter associated to the aggregation widget
+                    reject: ['Spain']
+                }
+            }]});
+            var rejectFilterList = rejectFilterMapConfig.getWidget(0, 'adm0name');
+            assert.equal(rejectFilterList.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (SELECT * FROM" +
+                    " (select * from populated_places_simple_reduced) " +
+                    "_cdb_category_filter WHERE adm0name NOT IN ('Spain')) " +
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
+
+            // check original mapconfig keeps it right
+            var list = mapConfig.getWidget(0, 'adm0name');
+            assert.equal(list.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (select * from populated_places_simple_reduced) " +
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
+        });
+
+        it('uses accept and reject filter param', function() {
+            var mapConfig = MapConfig.create(listsMapConfig);
+
+            var acceptAndRejectFilterMapConfig = mapConfig.applyFilters({layers: [{
+                adm0name: { // this is category filter associated to the aggregation widget
+                    reject: ['Spain'],
+                    accept: ['USA']
+                }
+            }]});
+            var acceptAndRejectFilterList = acceptAndRejectFilterMapConfig.getWidget(0, 'adm0name');
+
+            assert.equal(acceptAndRejectFilterList.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (SELECT * FROM" +
+                    " (select * from populated_places_simple_reduced) " +
+                    "_cdb_category_filter WHERE adm0name IN ('USA') AND adm0name NOT IN ('Spain')) " +
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
+
+            // check original mapconfig keeps it right
+            var list = mapConfig.getWidget(0, 'adm0name');
+            assert.equal(list.sql(),
+                    "SELECT count(*) AS count, adm0name FROM" +
+                    " (select * from populated_places_simple_reduced) " +
+                    "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+            );
+        });
     });
 
 });
