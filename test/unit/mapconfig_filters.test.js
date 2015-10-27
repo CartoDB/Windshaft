06db4fc71d54efcaae85f93e34829186177dfaf2
Raul Ochoa <rochoaf@gmail.com>
2015-10-27 17:36:50 +0100

    Adds tests for error cases in category filter

--- test/unit/mapconfig_filters.test.js
+++ test/unit/mapconfig_filters.test.js
@@ -5,32 +5,92 @@ var MapConfig = require('../../lib/windshaft/models/mapconfig');
 
 describe('mapconfig filters', function() {
 
-    describe('should return an object with filtered aggregations', function() {
-        var listsMapConfig = {
-            version: '1.5.0',
-            layers: [
-                {
-                    type: 'mapnik',
-                    options: {
-                        sql: 'select * from populated_places_simple_reduced',
-                        cartocss: '#layer0 { marker-fill: red; marker-width: 10; }',
-                        cartocss_version: '2.0.1',
-                        widgets: {
-                            adm0name: {
-                                type: 'aggregation',
-                                options: {
-                                    aggregation: 'count',
-                                    column: 'adm0name'
-                                }
+    var categoryWidgetMapConfig = {
+        version: '1.5.0',
+        layers: [
+            {
+                type: 'mapnik',
+                options: {
+                    sql: 'select * from populated_places_simple_reduced',
+                    cartocss: '#layer0 { marker-fill: red; marker-width: 10; }',
+                    cartocss_version: '2.0.1',
+                    widgets: {
+                        adm0name: {
+                            type: 'aggregation',
+                            options: {
+                                aggregation: 'count',
+                                column: 'adm0name'
                             }
                         }
                     }
                 }
-            ]
-        };
+            }
+        ]
+    };
+
+    describe('error', function() {
+        var mapConfig = MapConfig.create(categoryWidgetMapConfig);
+
+        it('fails to apply category filter if no params are used', function() {
+            assert.throws(
+                function() {
+                    mapConfig.applyFilters({layers: [{
+                        adm0name: {}
+                    }]});
+                },
+                function(err) {
+                    assert.equal(
+                        err.message,
+                        'Category filter expect to have at least one value in accept or reject params'
+                    );
+                    return true;
+                }
+            );
+        });
+
+        it('fails to apply category filter if no accept is empty', function() {
+            assert.throws(
+                function() {
+                    mapConfig.applyFilters({layers: [{
+                        adm0name: {
+                            accept: []
+                        }
+                    }]});
+                },
+                function(err) {
+                    assert.equal(
+                        err.message,
+                        'Category filter expect to have at least one value in accept or reject params'
+                    );
+                    return true;
+                }
+            );
+        });
+
+        it('fails to apply category filter if no reject is empty', function() {
+            assert.throws(
+                function() {
+                    mapConfig.applyFilters({layers: [{
+                        adm0name: {
+                            reject: []
+                        }
+                    }]});
+                },
+                function(err) {
+                    assert.equal(
+                        err.message,
+                        'Category filter expect to have at least one value in accept or reject params'
+                    );
+                    return true;
+                }
+            );
+        });
+    });
+
+    describe('should return an object with filtered aggregations', function() {
 
         it('uses accept filter param', function() {
-            var mapConfig = MapConfig.create(listsMapConfig);
+            var mapConfig = MapConfig.create(categoryWidgetMapConfig);
             var acceptFilterMapConfig = mapConfig.applyFilters({layers: [{
                 adm0name: { // this is category filter associated to the aggregation widget
                     accept: ['Spain']
@@ -56,7 +116,7 @@ describe('mapconfig filters', function() {
         });
 
         it('uses reject filter param', function() {
-            var mapConfig = MapConfig.create(listsMapConfig);
+            var mapConfig = MapConfig.create(categoryWidgetMapConfig);
 
             var rejectFilterMapConfig = mapConfig.applyFilters({layers: [{
                 adm0name: { // this is category filter associated to the aggregation widget
@@ -82,7 +142,7 @@ describe('mapconfig filters', function() {
         });
 
         it('uses accept and reject filter param', function() {
-            var mapConfig = MapConfig.create(listsMapConfig);
+            var mapConfig = MapConfig.create(categoryWidgetMapConfig);
 
             var acceptAndRejectFilterMapConfig = mapConfig.applyFilters({layers: [{
                 adm0name: { // this is category filter associated to the aggregation widget
