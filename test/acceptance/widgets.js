f4defd521fe9830694ebfe0de2440881deebdbcf
Raul Ochoa <rochoaf@gmail.com>
2015-10-28 20:08:42 +0100

    Move tests from histograms into widgets suite

--- test/acceptance/widgets.js
+++ test/acceptance/widgets.js
@@ -6,7 +6,7 @@ var TestClient = require('../support/test_client');
 describe('widgets', function() {
 
     describe('lists', function() {
-            
+
         var listsMapConfig = {
             version: '1.5.0',
             layers: [
@@ -56,5 +56,111 @@ describe('widgets', function() {
         });
 
     });
+
+    describe('histograms', function() {
+
+        var histogramsMapConfig = {
+            version: '1.5.0',
+            layers: [
+                {
+                    type: 'mapnik',
+                    options: {
+                        sql: 'select * from populated_places_simple_reduced',
+                        cartocss: '#layer0 { marker-fill: red; marker-width: 10; }',
+                        cartocss_version: '2.0.1',
+                        widgets: {
+                            scalerank: {
+                                type: 'histogram',
+                                options: {
+                                    column: 'scalerank'
+                                }
+                            },
+                            pop_max: {
+                                type: 'histogram',
+                                options: {
+                                    column: 'pop_max'
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        };
+
+        it('can be fetched from a valid histogram', function(done) {
+            var testClient = new TestClient(histogramsMapConfig);
+            testClient.getWidget(0, 'scalerank', function (err, histogram) {
+                assert.ok(!err, err);
+                assert.ok(histogram);
+
+                assert.ok(histogram.length);
+
+                assert.deepEqual(histogram[0], { bucket: 0, buckets: 10, min: 1, max: 1, freq: 179 });
+
+                done();
+            });
+        });
+
+        it('can be fetched from a valid histogram', function(done) {
+            var testClient = new TestClient(histogramsMapConfig);
+            testClient.getWidget(0, 'pop_max', function (err, histogram) {
+                assert.ok(!err, err);
+                assert.ok(histogram);
+
+                assert.ok(histogram.length);
+
+                assert.deepEqual(
+                    histogram[histogram.length - 1],
+                    { bucket: 9, buckets: 10, min: 35676000, max: 35676000, freq: 1 }
+                );
+
+                done();
+            });
+        });
+
+        it('returns array with undefined entries for empty bins', function(done) {
+            var histogram20binsMapConfig = {
+                version: '1.5.0',
+                layers: [
+                    {
+                        type: 'mapnik',
+                        options: {
+                            sql: 'select * from populated_places_simple_reduced',
+                            cartocss: '#layer0 { marker-fill: red; marker-width: 10; }',
+                            cartocss_version: '2.0.1',
+                            widgets: {
+                                pop_max: {
+                                    type: 'histogram',
+                                    options: {
+                                        column: 'pop_max',
+                                        bins: 20
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            };
+
+            var testClient = new TestClient(histogram20binsMapConfig);
+            testClient.getWidget(0, 'pop_max', function (err, histogram) {
+                assert.ok(!err, err);
+                assert.ok(histogram);
+
+                assert.ok(histogram.length);
+
+                assert.deepEqual(
+                    histogram[histogram.length - 1],
+                    { bucket: 19, buckets: 20, min: 35676000, max: 35676000, freq: 1 }
+                );
+
+                assert.ok(!histogram[histogram.length - 2]);
+
+                done();
+            });
+        });
+
+    });
+
 
 });
