2843effc0e19bfc1eedcb0e275b565d14c161e10
Raul Ochoa <rochoaf@gmail.com>
2015-11-16 16:41:52 +0100

    Fix filters tests

--- test/unit/mapconfig_filters.test.js
+++ test/unit/mapconfig_filters.test.js
@@ -106,20 +106,51 @@ describe('mapconfig filters', function() {
                 }]});
 
                 assert.equal(mapConfig.getLayer(0).options.sql,
-                    "SELECT * FROM (" +
-                        "select * from populated_places_simple_reduced" +
-                    ") _cdb_category_filter WHERE adm0name IN ('Spain')"
+                    [
+                        'SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name IN ($string_0$Spain$string_0$)'
+                    ].join('\n')
                 );
 
                 assert.notEqual(mapConfig.id(), mapConfigId);
 
                 var acceptFilterAggregation = mapConfig.getWidget(0, 'adm0name');
                 assert.equal(acceptFilterAggregation.sql(mapConfig.getLayerFilters(0)),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (SELECT * FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_category_filter WHERE adm0name IN ('Spain')) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name IN ($string_0$Spain$string_0$)) _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name IN ($string_0$Spain$string_0$)) _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
                 );
 
                 mapConfig.clearFilters();
@@ -128,9 +159,37 @@ describe('mapconfig filters', function() {
                 // check original mapconfig keeps it right
                 var aggregation = mapConfig.getWidget(0, 'adm0name');
                 assert.equal(aggregation.sql(),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
+
                 );
             });
 
@@ -144,20 +203,78 @@ describe('mapconfig filters', function() {
                 }]});
                 var rejectFilterAggregation = mapConfig.getWidget(0, 'adm0name');
                 assert.equal(rejectFilterAggregation.sql(mapConfig.getLayerFilters(0)),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (SELECT * FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_category_filter WHERE adm0name NOT IN ('Spain')) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name NOT IN ($string_0$Spain$string_0$)) _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name NOT IN ($string_0$Spain$string_0$)) _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
+
                 );
 
                 // check original mapconfig keeps it right
                 mapConfig.clearFilters();
                 var aggregation = mapConfig.getWidget(0, 'adm0name');
                 assert.equal(aggregation.sql(),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
+
                 );
             });
 
@@ -173,20 +290,80 @@ describe('mapconfig filters', function() {
                 var acceptAndRejectFilterAggregation = mapConfig.getWidget(0, 'adm0name');
 
                 assert.equal(acceptAndRejectFilterAggregation.sql(mapConfig.getLayerFilters(0)),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (SELECT * FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_category_filter WHERE adm0name IN ('USA') AND adm0name NOT IN ('Spain')) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name IN ($string_0$USA$string_0$) AND adm0name NOT IN ($string_0$Spain$string_0$))' +
+                            ' _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (SELECT *',
+                        'FROM (select * from populated_places_simple_reduced) _cdb_category_filter',
+                        'WHERE adm0name IN ($string_0$USA$string_0$) AND adm0name NOT IN ($string_0$Spain$string_0$))' +
+                            ' _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
+
                 );
 
                 // check original mapconfig keeps it right
                 mapConfig.clearFilters();
                 var aggregation = mapConfig.getWidget(0, 'adm0name');
                 assert.equal(aggregation.sql(),
-                        "SELECT count(*) AS count, adm0name FROM" +
-                        " (select * from populated_places_simple_reduced) " +
-                        "_cdb_aggregation GROUP BY adm0name ORDER BY count DESC"
+                    [
+                        'WITH',
+                        'summary AS (',
+                        '  SELECT',
+                        '  count(1) AS count,',
+                        '  sum(CASE WHEN adm0name IS NULL THEN 1 ELSE 0 END) AS nulls_count',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_nulls',
+                        '),',
+                        'categories AS(',
+                        '  SELECT adm0name AS category, count(1) AS value, row_number() OVER (ORDER BY count(1) desc)' +
+                            ' as rank',
+                        '  FROM (select * from populated_places_simple_reduced) _cdb_aggregation_all',
+                        '  GROUP BY adm0name',
+                        '  ORDER BY 2 DESC',
+                        '),',
+                        'categories_summary AS(',
+                        '  SELECT count(1) categories_count, max(value) max_val, min(value) min_val',
+                        '  FROM categories',
+                        ')',
+                        'SELECT CAST(category AS text), value, false as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank < 12',
+                        'UNION ALL',
+                        'SELECT \'Other\' category, sum(value), true as agg, nulls_count, min_val, max_val, count,' +
+                            ' categories_count',
+                        '  FROM categories, summary, categories_summary',
+                        '  WHERE rank >= 12',
+                        'GROUP BY nulls_count, min_val, max_val, count, categories_count'
+                    ].join('\n')
+
                 );
             });
         });
