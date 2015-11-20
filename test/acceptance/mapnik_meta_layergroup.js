450dc3584d6e8c040d2fd21147a09b4f37d0028a
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-20 13:27:37 +0100

    Implemented more test for layergroup metadata collection

--- test/acceptance/mapnik_meta_layergroup.js
+++ test/acceptance/mapnik_meta_layergroup.js
@@ -1,17 +1,58 @@
 require('../support/test_helper');
 
-var _ = require('underscore');
 var assert = require('../support/assert');
 var TestClient = require('../support/test_client');
 
 describe('Create mapnik layergroup', function() {
-    var mapConfig = TestClient.singleLayerMapConfig('select * from test_table limit 2');
+    var cartocssVersion = '2.3.0';
+    var cartocss = '#layer { line-width:16; }';
 
-    it('should response with meta-stats', function(done) {
-        var testClient = new TestClient(mapConfig, {
-            mapnik: {
-                mapnik: _.extend({}, TestClient.mapnikOptions)
-            }
+    var mapnikLayer1 = {
+        type: 'mapnik',
+        options: {
+            sql: 'select * from test_table limit 2',
+            cartocss_version: cartocssVersion,
+            cartocss: cartocss
+        }
+    };
+    
+    var mapnikLayer2 = {
+        type: 'mapnik',
+        options: {
+            sql: 'select * from test_table_2 limit 2',
+            cartocss_version: cartocssVersion,
+            cartocss: cartocss
+        }
+    };
+
+    var mapnikLayer3 = {
+        type: 'mapnik',
+        options: {
+            sql: 'select * from test_table_3 limit 2',
+            cartocss_version: cartocssVersion,
+            cartocss: cartocss
+        }
+    };
+    
+    var mapnikLayer4 = {
+        type: 'mapnik',
+        options: {
+            sql: [
+                'select t1.cartodb_id, t1.the_geom, t2.address',
+                ' from test_table t1, test_table_2 t2',
+                ' where t1.cartodb_id = t2.cartodb_id;'
+            ].join(''),
+            cartocss_version: cartocssVersion,
+            cartocss: cartocss
+        }
+    };
+    
+    it('with one mapnik layer should response with meta-stats for that layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer1
+            ]
         });
         
         testClient.createLayergroup(function(err, layergroup) {
@@ -20,5 +61,58 @@ describe('Create mapnik layergroup', function() {
             done();
         });
     });
+    
+    it('with two mapnik layer should response with meta-stats for every layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer1,
+                mapnikLayer2
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[0].features === 5);
+            done();
+        });
+    });
 
+    it('with three mapnik layer should response with meta-stats for every layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer1,
+                mapnikLayer2,
+                mapnikLayer3
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[2].meta.stats[0].features === 5);
+            done();
+        });
+    });
+    
+    it('with one mapnik layer (sql with join) should response with meta-stats for that layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer4
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[1].features === 5);
+            done();
+        });
+    });
+    
+    
 });
