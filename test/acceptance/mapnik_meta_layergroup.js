344e7dcfd9c47df0e51e90df9c96bc9b389124ed
Daniel García Aubert <dgaubert@cartodb.com>
2015-11-20 15:27:49 +0100

    Added more tests for metadata stats

--- test/acceptance/mapnik_meta_layergroup.js
+++ test/acceptance/mapnik_meta_layergroup.js
@@ -3,7 +3,7 @@ require('../support/test_helper');
 var assert = require('../support/assert');
 var TestClient = require('../support/test_client');
 
-describe('Create mapnik layergroup', function() {
+describe.only('Create mapnik layergroup', function() {
     var cartocssVersion = '2.3.0';
     var cartocss = '#layer { line-width:16; }';
 
@@ -44,6 +44,14 @@ describe('Create mapnik layergroup', function() {
             ].join(''),
             cartocss_version: cartocssVersion,
             cartocss: cartocss
+        }
+    };
+    
+    var httpLayer = {
+        type: 'http',
+        options: {
+            urlTemplate: 'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',
+            subdomains: ['a','b','c']
         }
     };
     
@@ -114,5 +122,100 @@ describe('Create mapnik layergroup', function() {
         });
     });
     
+    it('with two mapnik layer (sql with join) should response with meta-stats for every layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer4,
+                mapnikLayer4
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[1].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[1].features === 5);
+            done();
+        });
+    });
     
+    it('with two mapnik layer (with & without join) should response with meta-stats for every layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer3,
+                mapnikLayer4
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(!layergroup.metadata.layers[0].meta.stats[1]);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[1].features === 5);
+            assert.ok(!layergroup.metadata.layers[1].meta.stats[2]);
+            assert.ok(!layergroup.metadata.layers[2]);
+            done();
+        });
+    });
+    
+    it('with two mapnik layer (with & without join) should response with meta-stats for every layer', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer3,
+                mapnikLayer4
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.ok(!layergroup.metadata.layers[0].meta.stats[1]);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[0].features === 5);
+            assert.ok(layergroup.metadata.layers[1].meta.stats[1].features === 5);
+            assert.ok(!layergroup.metadata.layers[1].meta.stats[2]);
+            assert.ok(!layergroup.metadata.layers[2]);
+            done();
+        });
+    });
+    
+    it('with mapnik and layer and httplayer should response with layer metadata with same order', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                mapnikLayer1,
+                httpLayer
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.equal(layergroup.metadata.layers[0].type, 'mapnik');
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.equal(layergroup.metadata.layers[1].type, 'http');
+            done();
+        });
+    });
+
+    it('with httpLayer and mapnik layer should response with layer metadata with same order', function(done) {
+        var testClient = new TestClient({
+            version: '1.4.0',
+            layers: [
+                httpLayer,
+                mapnikLayer1
+            ]
+        });
+        
+        testClient.createLayergroup(function(err, layergroup) {
+            assert.ok(!err);
+            assert.equal(layergroup.metadata.layers[0].type, 'http');
+            assert.ok(layergroup.metadata.layers[0].meta.stats[0].features === 5);
+            assert.equal(layergroup.metadata.layers[1].type, 'mapnik');
+            done();
+        });
+    });
 });
