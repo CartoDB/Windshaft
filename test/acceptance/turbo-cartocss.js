07d2b2ad954fc700380033d92539c2ee47f27caf
Daniel García Aubert <dgaubert@cartodb.com>
2016-02-18 12:43:46 +0100

    Fixed turbo-cartocss intregation for torque renderer

--- test/acceptance/turbo-cartocss.js
+++ test/acceptance/turbo-cartocss.js
@@ -4,6 +4,7 @@ var assert = require('../support/assert');
 var TestClient = require('../support/test_client');
 
 var IMAGE_EQUALS_TOLERANCE_PER_MIL = 1;
+var IMAGE_TOLERANCE_PER_MIL = 20;
 
 function imageCompareFn(fixture, done) {
     return function(err, tile) {
@@ -15,7 +16,7 @@ function imageCompareFn(fixture, done) {
 }
 
 describe('turbo-cartocss', function() {
-    describe('parsing ramp function with colorbrewer for greens', function () {
+    describe('parsing ramp function with colorbrewer for greens and mapnik renderer', function () {
         before(function (done) {
             this.testClient = new TestClient({
                 "version": "1.4.0",
@@ -40,7 +41,7 @@ describe('turbo-cartocss', function() {
         });
     });
 
-    describe('parsing ramp function with colorbrewer for reds', function () {
+    describe('parsing ramp function with colorbrewer for reds and mapnik renderer', function () {
 
         before(function (done) {
             this.testClient = new TestClient({
@@ -63,6 +64,63 @@ describe('turbo-cartocss', function() {
         it('should get a tile with turbo-cartocss parsed properly', function (done) {
             this.testClient.getTile(13, 4011, 3088,
                 imageCompareFn('test_turbo_cartocss_reds_13_4011_3088.png', done));
+        });
+    });
+
+    describe('parsing ramp function with colorbrewer for greens and toque renderer', function () {
+        before(function (done) {
+            this.testClient = new TestClient({
+                version: '1.2.0',
+                layers: [
+                    {
+                        type: 'torque',
+                        options: {
+                            sql: "SELECT * FROM populated_places_simple_reduced where the_geom" +
+                                " && ST_MakeEnvelope(-90, 0, 90, 65)",
+                            cartocss: [
+                                'Map {',
+                                '    buffer-size:0;',
+                                '    -torque-frame-count:1;',
+                                '    -torque-animation-duration:30;',
+                                '    -torque-time-attribute:"cartodb_id";',
+                                '    -torque-aggregation-function:"count(cartodb_id)";',
+                                '    -torque-resolution:1;',
+                                '    -torque-data-aggregation:linear;',
+                                '};',
+                                '#populated_places_simple_reduced {',
+                                '    comp-op: multiply;',
+                                '    marker-fill-opacity: 1;',
+                                '    marker-line-color: #FFF;',
+                                '    marker-line-width: 0;',
+                                '    marker-line-opacity: 1;',
+                                '    marker-type: rectangle;',
+                                '    marker-width: 3;',
+                                '    marker-fill: ramp([pop_max], colorbrewer(Greens), jenks);',
+                                '};'
+                            ].join(' '),
+                            cartocss_version: '2.3.0'
+                        }
+                    }
+                ]
+            });
+
+            this.testClient.createLayergroup(done);
+        });
+
+        it('should get a tile with turbo-cartocss parsed properly', function (done) {
+            var z = 2;
+            var x = 2;
+            var y = 1;
+
+            this.testClient.getTile(z, x, y, { layer: 0 }, function (err, tile) {
+                var pngFixture = './test/fixtures/torque/populated_places_simple_reduced-turbo-cartocss-' +
+                [z, x, y].join('.') + '.png';
+                require('fs').writeFileSync('torque.png', tile);
+                assert.imageEqualsFile(tile, pngFixture, IMAGE_TOLERANCE_PER_MIL, function (err) {
+                    assert.ok(!err);
+                    done();
+                });
+            });
         });
     });
 });
