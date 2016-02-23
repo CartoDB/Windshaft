65557baab6675811ffd5cf195e8c8529d26d470f
Francisco Dans <fdansv@gmail.com>
2016-02-23 12:53:45 +0100

    passes buffer distance calculation to sql query

--- lib/windshaft/renderers/mapnik/geojson_template.sql
+++ lib/windshaft/renderers/mapnik/geojson_template.sql
@@ -1,9 +1,9 @@
 SELECT row_to_json(featcoll) as geojson FROM
   (SELECT 'FeatureCollection' AS TYPE, array_to_json(array_agg(feat)) AS features FROM
-    (SELECT 'Feature' AS TYPE, ST_AsGeoJSON({{ if (it.clipByBox2d) { }}ST_ClipByBox2D{{ } else { }}ST_Intersection{{ } }}(tbl.{{= it.geomColumn }}, st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), {{= it.bufferDistance}})))::json AS geometry, {{ if (!it.columns || it.columns.length === 0) { }} '{}'::json {{ } else { }} row_to_json(
+    (SELECT 'Feature' AS TYPE, ST_AsGeoJSON({{ if (it.clipByBox2d) { }}ST_ClipByBox2D{{ } else { }}ST_Intersection{{ } }}(tbl.{{= it.geomColumn }}, st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}})))::json AS geometry, {{ if (!it.columns || it.columns.length === 0) { }} '{}'::json {{ } else { }} row_to_json(
       (SELECT l FROM
         (SELECT {{ for (var i = 0; i < it.columns.length; i++) { }} {{= it.columns[i] }} {{ if (i < (it.columns.length - 1)) { }}, {{ } }} {{ } }}) AS l
       )
     ) {{ } }} AS properties
     FROM ({{= it.layerSql }}) AS tbl
-    WHERE {{= it.geomColumn }} && st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), {{= it.bufferDistance}})) AS feat) AS featcoll;
+    WHERE {{= it.geomColumn }} && st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}})) AS feat) AS featcoll;
