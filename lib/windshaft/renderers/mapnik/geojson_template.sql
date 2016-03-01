c9b06ca606b3dc675c06c49b0f01ac1af8c9e5e6
Daniel García Aubert <dgaubert@cartodb.com>
2016-03-01 16:11:17 +0100

    Improved geojson query in order to simplify geometries

--- lib/windshaft/renderers/mapnik/geojson_template.sql
+++ lib/windshaft/renderers/mapnik/geojson_template.sql
@@ -1,9 +1,57 @@
-SELECT row_to_json(featcoll) as geojson FROM
-  (SELECT 'FeatureCollection' AS TYPE, array_to_json(array_agg(feat)) AS features FROM
-    (SELECT 'Feature' AS TYPE, ST_AsGeoJSON({{ if (it.clipByBox2d) { }}ST_ClipByBox2D{{ } else { }}ST_Intersection{{ } }}(st_makevalid(tbl.{{= it.geomColumn }}), st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}})))::json AS geometry, {{ if (!it.columns || it.columns.length === 0) { }} '{}'::json {{ } else { }} row_to_json(
-      (SELECT l FROM
-        (SELECT {{ for (var i = 0; i < it.columns.length; i++) { }} {{= it.columns[i] }} {{ if (i < (it.columns.length - 1)) { }}, {{ } }} {{ } }}) AS l
+SELECT
+  row_to_json(featurecollection) as geojson
+FROM (
+  SELECT
+    'FeatureCollection' AS TYPE,
+    array_to_json(
+      array_agg(feature)
+    ) AS features
+  FROM (
+    SELECT
+      'Feature' AS TYPE,
+      st_asgeojson(
+        {{ if (it.clipByBox2d) { }}
+        st_clipbybox2d(
+        {{ } else { }}
+        st_intersection(
+        {{ } }}
+          st_simplify(
+            st_makevalid(tbl.{{= it.geomColumn }}),
+            cdb_xyz_resolution({{= it.zoom }}) * (1.0 / 20.0),
+            true
+          ),
+          st_expand(
+            cdb_xyz_extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
+            cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
+          )
+        )
+      )::json AS geometry,
+      {{ if (!it.columns || it.columns.length === 0) { }}
+      '{}'::json
+      {{ } else { }}
+      row_to_json(
+        (
+          SELECT
+            l
+          FROM (
+            SELECT
+              {{ for (var i = 0; i < it.columns.length; i++) { }}
+                {{= it.columns[i] }}{{ if (i < (it.columns.length - 1)) { }},{{ } }}
+              {{ } }}
+          ) AS l
+        )
       )
-    ) {{ } }} AS properties
-    FROM ({{= it.layerSql }}) AS tbl
-    WHERE {{= it.geomColumn }} && st_expand(CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}), cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}})) AS feat) AS featcoll;
+      {{ } }}
+      AS properties
+    FROM
+      ({{= it.layerSql }}) AS tbl
+    WHERE (
+      {{= it.geomColumn }}
+      &&
+      st_expand(
+        cdb_xyz_extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
+        cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
+      )
+    )
+  ) AS feature
+) AS featurecollection;
