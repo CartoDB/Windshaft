93c7e50769389d1548a08e4233686d2adc13d4d1
Raul Ochoa <rochoaf@gmail.com>
2016-03-09 18:15:17 +0000

    Match some names from master

--- lib/windshaft/renderers/mapnik/geojson_template.sql
+++ lib/windshaft/renderers/mapnik/geojson_template.sql
@@ -1,13 +1,13 @@
-SELECT row_to_json(featurecollection) as geojson
+SELECT row_to_json(featcoll) as geojson
 FROM (
     SELECT 'FeatureCollection' AS TYPE,
-        array_to_json(array_agg(feature)) AS features
+        array_to_json(array_agg(feat)) AS features
         FROM (
             SELECT 'Feature' AS TYPE, st_asgeojson(
                 {{ if (it.clipByBox2d) { }}
-                    st_clipbybox2d
+                    ST_ClipByBox2D
                 {{ } else { }}
-                    st_intersection
+                    ST_Intersection
                 {{ } }}
                 (
                     st_makevalid(
@@ -23,7 +23,7 @@ FROM (
                         )
                     ),
                     st_expand(
-                        cdb_xyz_extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
+                        CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
                         cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
                     )
                 )
@@ -33,7 +33,7 @@ FROM (
             {{ } else { }}
                 row_to_json(
                     (SELECT l FROM
-                        (SELECT {{ for (var i = 0; i < it.columns.length; i++) { }} {{= it.columns[i] }}{{ if (i < (it.columns.length - 1)) { }}, {{ } }} {{ } }}) AS l
+                        (SELECT {{ for (var i = 0; i < it.columns.length; i++) { }} {{= it.columns[i] }} {{ if (i < (it.columns.length - 1)) { }}, {{ } }} {{ } }}) AS l
                     )
                 )
             {{ } }} AS properties
@@ -42,11 +42,11 @@ FROM (
                 st_intersects(
                     {{= it.geomColumn }},
                     st_expand(
-                        cdb_xyz_extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
+                        CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
                         cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
                     )
                 )
             )
-        ) AS feature
+        ) AS feat
     )
-AS featurecollection;
+AS featcoll;
