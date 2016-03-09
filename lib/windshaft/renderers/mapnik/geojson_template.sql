9c6fc572c2a859c4b43f3131e4b02b9bb3422fed
Raul Ochoa <rochoaf@gmail.com>
2016-03-09 20:50:48 +0000

    Calculate clip function outside

--- lib/windshaft/renderers/mapnik/geojson_template.sql
+++ lib/windshaft/renderers/mapnik/geojson_template.sql
@@ -4,22 +4,15 @@ FROM (
         array_to_json(array_agg(feat)) AS features
         FROM (
             SELECT 'Feature' AS TYPE,
-            ST_AsGeoJSON(
-                {{ if (it.clipByBox2d) { }}
-                    ST_ClipByBox2D
-                {{ } else { }}
-                    ST_Intersection
-                {{ } }}
-                (
-                    st_makevalid(
-                        tbl.{{= it.geomColumn }}
-                    ),
-                    st_expand(
-                        CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
-                        cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
-                    )
+            ST_AsGeoJSON({{=it.clipFn}}(
+                st_makevalid(
+                    tbl.{{= it.geomColumn }}
+                ),
+                st_expand(
+                    CDB_XYZ_Extent({{= it.coord.x }}, {{= it.coord.y }}, {{= it.zoom }}),
+                    cdb_xyz_resolution({{= it.zoom }}) * {{= it.bufferSize}}
                 )
-            )::json AS geometry,
+            ))::json AS geometry,
             {{ if (!it.columns || it.columns.length === 0) { }}
                 '{}'::json
             {{ } else { }}
