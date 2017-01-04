735c5aa7bd1eba143aecb0b0368b727547ea00c7
Daniel García Aubert <dgaubert@cartodb.com>
2017-01-04 17:38:24 +0100

    Implemented function to return the maximum value of buffer-size used in cartocss of a map

--- test/unit/utils/cartocss_utils.test.js
+++ test/unit/utils/cartocss_utils.test.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert');
 var cartocssUtils = require('../../../lib/windshaft/utils/cartocss_utils');
+var MapConfig = require('../../../lib/windshaft/models/mapconfig');
 
 describe('cartocss utils', function () {
     describe('.getColumnNamesFromCartoCSS()', function () {
@@ -221,5 +222,94 @@ describe('cartocss utils', function () {
             assert.deepEqual(columns, []);
         });
 
+    });
+
+    describe('.getMaxBufferSizeFromCartoCSS()', function () {
+        var scenarios = [{
+            description: 'happy case should return "buffer-size: 128"',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 128;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 128
+        }, {
+            description: 'shoud take into account both "mapnik" and "cartodb" layers',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 128;',
+                '}'
+            ].join('\n'), [
+                'Map {',
+                '  buffer-size: 64;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 128
+        }, {
+            description: 'two layers should return max buffer-size',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 128;',
+                '}'
+            ].join('\n'), [
+                'Map {',
+                '  buffer-size: 256;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 256
+        }, {
+            description: 'three layers should return max buffer-size',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 128;',
+                '}'
+            ].join('\n'), [
+                'Map {',
+                '  buffer-size: 64;',
+                '}'
+            ].join('\n'), [
+                'Map {',
+                '  buffer-size: 64;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 128
+        }, {
+            description: 'two Map definitions in one layer should return the last defined',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 128;',
+                '}',
+                'Map {',
+                '  buffer-size: 64;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 64
+        }, {
+            description: 'two buffer-size definitions in one layer should return the last defined',
+            cartocss: [[
+                'Map {',
+                '  buffer-size: 256;',
+                '  buffer-size: 128;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 128
+        }, {
+            description: 'should return 0 if buffer-size is not defined',
+            cartocss: [[
+                '#layer {',
+                '  marker-width: 2;',
+                '}'
+            ].join('\n')],
+            expectedBufferSize: 0
+        }];
+
+        scenarios.forEach(function (scenario) {
+            var test = scenario.only ? it.only : it;
+
+            test(scenario.description, function () {
+                var bufferSize = cartocssUtils.getMaxBufferSizeFromCartoCSS(scenario.cartocss);
+                assert.equal(bufferSize, scenario.expectedBufferSize);
+            });
+        });
     });
 });
