a47342cda85e003464145a8767c3a355c795161d
Rafa de la Torre <rtorre@carto.com>
2017-06-13 11:29:05 +0200

    Better integration tests

--- test/integration/mapnik_timer_stats.js
+++ test/integration/mapnik_timer_stats.js
@@ -1,17 +1,37 @@
+require('../support/test_helper');
+
 var assert = require('../support/assert');
 var windshaft = require('../../lib/windshaft');
+var TestClient = require('../support/test_client');
 
-var mapnik = require('mapnik');
+describe('stats.MapnikTimer', function() {
 
-describe('mapnik_timer', function() {
-
-    it('should wrap around mapnik TimerStats and return the same', function(done) {
-        assert.equal(windshaft.stats.MapnikTimer.flush(), mapnik.TimerStats.flush());
+    before(function(done) {
+        windshaft.stats.MapnikTimer.flush();
         done();
     });
 
-    it('returns nothing when no map is rendered', function(done) {
-        assert.equal(windshaft.stats.MapnikTimer.flush(), '');
+    after(function(done) {
+        windshaft.stats.MapnikTimer.flush();
         done();
+    });
+
+    describe('flush() when no map is rendered', function() {
+        it('returns nothing when no map is rendered', function(done) {
+            assert.equal(windshaft.stats.MapnikTimer.flush(), '');
+            done();
+        });
+    });
+
+    describe('flush() when an image is rendered', function() {
+        it('should return some stats when an image is rendered', function(done) {
+            var testClient = new TestClient(TestClient.defaultTableMapConfig('test_table'));
+            testClient.getTile(13, 4011, 3088, {cache_buster: 'wadus'}, function (err, tile, img, headers) {
+                assert.ok(!err);
+                assert.notEqual(windshaft.stats.MapnikTimer.flush());
+                done();
+            });
+
+        });
     });
 });
