0bba2f543050299ddc8162ed74765b145905e569
Raul Ochoa <rochoaf@gmail.com>
2017-10-04 15:51:18 +0000

    Fix tests

--- test/unit/utils/layer-columns.js
+++ test/unit/utils/layer-columns.js
@@ -1,5 +1,5 @@
 require('../../support/test_helper.js');
-var geojsonUtils = require('../../../lib/windshaft/utils/layer-columns');
+var LayerColumns = require('../../../lib/windshaft/utils/layer-columns');
 var assert = require('assert');
 
 describe('geojson-utils', function() {
@@ -28,47 +28,47 @@ describe('geojson-utils', function() {
     }
 
     it('should not duplicate column names', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions());
-        assert.deepEqual(properties, ['pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions());
+        assert.deepEqual(columns, ['pop_max', 'name']);
     });
 
     it('should handle interactivity strings', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions('cartodb_id,pop_max'));
-        assert.deepEqual(properties, ['pop_max', 'name', 'cartodb_id']);
+        var columns = LayerColumns.getColumns(createOptions('cartodb_id,pop_max'));
+        assert.deepEqual(columns, ['pop_max', 'name', 'cartodb_id']);
     });
 
     it('should handle interactivity array', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(['cartodb_id', 'pop_max']));
-        assert.deepEqual(properties, ['pop_max', 'name', 'cartodb_id']);
+        var columns = LayerColumns.getColumns(createOptions(['cartodb_id', 'pop_max']));
+        assert.deepEqual(columns, ['pop_max', 'name', 'cartodb_id']);
     });
 
     it('should handle columns array', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null, ['cartodb_id', 'pop_min']));
-        assert.deepEqual(properties, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null, ['cartodb_id', 'pop_min']));
+        assert.deepEqual(columns, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
     });
 
     it('should handle empty columns array', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null, []));
-        assert.deepEqual(properties, ['pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null, []));
+        assert.deepEqual(columns, ['pop_max', 'name']);
     });
 
     it('should ignore no-string values', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null, ['cartodb_id', 'pop_min', 1]));
-        assert.deepEqual(properties, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null, ['cartodb_id', 'pop_min', 1]));
+        assert.deepEqual(columns, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
     });
 
     it('should ignore null values', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null, ['cartodb_id', 'pop_min', null]));
-        assert.deepEqual(properties, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null, ['cartodb_id', 'pop_min', null]));
+        assert.deepEqual(columns, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
     });
 
     it('should ignore undefined values', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null,['cartodb_id',undefined,'pop_min']));
-        assert.deepEqual(properties, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null,['cartodb_id',undefined,'pop_min']));
+        assert.deepEqual(columns, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
     });
 
     it('should ignore empty values', function() {
-        var properties = geojsonUtils.getGeojsonProperties(createOptions(null,['cartodb_id', '' ,'pop_min']));
-        assert.deepEqual(properties, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
+        var columns = LayerColumns.getColumns(createOptions(null,['cartodb_id', '' ,'pop_min']));
+        assert.deepEqual(columns, ['cartodb_id', 'pop_min', 'pop_max', 'name']);
     });
 });
