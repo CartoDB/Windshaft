7de991568358e4608e52d44b27555f38961ca59a
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-27 18:14:55 +0100

    Validate layers: now you can't mix styled layers with non styled layers (without cartocss)

--- test/acceptance/vector-aggregation.js
+++ test/acceptance/vector-aggregation.js
@@ -2,13 +2,15 @@ require('../support/test_helper');
 
 var assert = require('../support/assert');
 var TestClient = require('../support/test_client');
+const INCOMPATIBLE_LAYERS_ERROR =
+'The layergroup contains incompatible layers: don\'t mix styled layers with non styled layers (without cartocss)';
 
 describe('vector aggregation', function () {
     it('should create a single layer map w/o cartocss', function (done) {
         this.testClient = new TestClient({
             version: '1.6.0',
             layers: [
-               {
+                {
                     type: 'cartodb',
                     options: {
                         sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
@@ -25,6 +27,34 @@ describe('vector aggregation', function () {
             assert.ok(layergroup);
             assert.ok(layergroup.layergroupid);
 
+            done();
+        });
+    });
+
+    it('should fail whether layergroup cantains incompatible layers', function (done) {
+        this.testClient = new TestClient({
+            version: '1.6.0',
+            layers: [
+                {
+                    type: 'cartodb',
+                    options: {
+                        sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
+                    }
+                },
+                {
+                    type: 'cartodb',
+                    options: {
+                        sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom',
+                        cartocss: '#layer { marker-fill:blue; marker-allow-overlap:true; }',
+                        cartocss_version: '2.0.2',
+                    }
+                }
+            ]
+        });
+
+        this.testClient.createLayergroup((err) => {
+            assert.ok(err);
+            assert.equal(INCOMPATIBLE_LAYERS_ERROR, err.message);
             done();
         });
     });
