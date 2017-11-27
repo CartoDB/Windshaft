27de7e122fb1bab93b625c65bdeb012678dacbca
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-27 18:30:55 +0100

    Add test to fetch tile for a layergroup w/o styles

--- test/acceptance/vector-aggregation.js
+++ test/acceptance/vector-aggregation.js
@@ -1,7 +1,8 @@
 require('../support/test_helper');
 
-var assert = require('../support/assert');
-var TestClient = require('../support/test_client');
+const assert = require('../support/assert');
+const mapnik = require('mapnik');
+const TestClient = require('../support/test_client');
 const INCOMPATIBLE_LAYERS_ERROR =
 'The layergroup contains incompatible layers: don\'t mix styled layers with non styled layers (without cartocss)';
 
@@ -56,6 +57,31 @@ describe('vector aggregation', function () {
             assert.ok(err);
             assert.equal(INCOMPATIBLE_LAYERS_ERROR, err.message);
             done();
+        });
+    });
+
+    it('should get tile 0/0/0 for a vector layergroup', function (done) {
+        this.testClient = new TestClient({
+            version: '1.6.0',
+            layers: [
+                {
+                    type: 'cartodb',
+                    options: {
+                        sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
+                    }
+                }
+            ]
+        });
+
+        this.testClient.getTile(0, 0, 0, (err, mvtTile) => {
+            if (err) {
+                return done(err);
+            }
+
+            var vtile = new mapnik.VectorTile(0, 0, 0);
+            vtile.setData(mvtTile);
+            assert.equal(vtile.painted(), true);
+            assert.equal(vtile.empty(), false);
         });
     });
 });
