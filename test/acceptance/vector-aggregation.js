e0cbbafdf7a65537337c3ca185b6fd2f9bac4e59
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-27 14:43:50 +0100

    vr-aggregation: first acceptance test to create a layergroup without cartocss

---
+++ test/acceptance/vector-aggregation.js
@@ -0,0 +1,31 @@
+require('../support/test_helper');
+
+var assert = require('../support/assert');
+var TestClient = require('../support/test_client');
+
+describe('vector aggregation', function () {
+    it.only('should create a single layer map w/o cartocss', function (done) {
+        this.testClient = new TestClient({
+            version: '1.6.0',
+            layers: [
+               {
+                    type: 'cartodb',
+                    options: {
+                        sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
+                    }
+                }
+            ]
+        });
+
+        this.testClient.createLayergroup((err, layergroup) => {
+            if (err) {
+                return done(err);
+            }
+
+            assert.ok(layergroup);
+            assert.ok(layergroup.layergroupid);
+
+            done();
+        });
+    });
+});
