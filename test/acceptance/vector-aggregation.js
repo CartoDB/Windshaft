af7279ecd9575b21cf02757625656cc4884d75d0
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-29 17:08:07 +0100

    Test MVT using postgis (ST_AsMVT)

--- test/acceptance/vector-aggregation.js
+++ test/acceptance/vector-aggregation.js
@@ -17,106 +17,121 @@ const POINTS_SQL = `
     from generate_series(-3, 3) x
 `;
 
-describe('vector aggregation', function () {
-    it('should create a single layer map w/o cartocss', function (done) {
-        this.testClient = new TestClient({
-            version: '1.6.0',
-            layers: [
-                {
-                    type: 'cartodb',
-                    options: {
-                        sql: POINTS_SQL
-                    }
-                }
-            ]
-        });
 
-        this.testClient.createLayergroup((err, layergroup) => {
-            if (err) {
-                return done(err);
-            }
-
-            assert.ok(layergroup);
-            assert.ok(layergroup.layergroupid);
-
-            done();
-        });
-    });
-
-    it('should fail whether layergroup cantains incompatible layers', function (done) {
-        this.testClient = new TestClient({
-            version: '1.6.0',
-            layers: [
-                {
-                    type: 'cartodb',
-                    options: {
-                        sql: POINTS_SQL
-                    }
-                },
-                {
-                    type: 'cartodb',
-                    options: {
-                        sql: POINTS_SQL,
-                        cartocss: '#layer { marker-fill:blue; marker-allow-overlap:true; }',
-                        cartocss_version: '2.0.2',
-                    }
-                }
-            ]
-        });
-
-        this.testClient.createLayergroup((err) => {
-            assert.ok(err);
-            assert.equal(INCOMPATIBLE_LAYERS_ERROR, err.message);
-            done();
-        });
-    });
-
-    it('should get tile 0/0/0 for a vector layergroup', function (done) {
-        this.testClient = new TestClient({
-            version: '1.6.0',
-            layers: [
-                {
-                    type: 'cartodb',
-                    options: {
-                        sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
-                    }
-                }
-            ]
-        });
-
-        this.testClient.getTile(0, 0, 0, { format: 'mvt' }, (err, mvtTile) => {
-            if (err) {
-                return done(err);
-            }
-
-            var vtile = new mapnik.VectorTile(0, 0, 0);
-            vtile.setData(mvtTile);
-            assert.equal(vtile.painted(), true);
-            assert.equal(vtile.empty(), false);
-            done();
-        });
-    });
-
-    it('should fail when requesting a raster format tile for a vector layergroup', function (done) {
-        this.testClient = new TestClient({
-            version: '1.6.0',
-            layers: [
-                {
-                    type: 'cartodb',
-                    options: {
-                        sql: POLYGONS_SQL,
-
-                    }
-                }
-            ]
-        });
-
-        this.testClient.getTile(0, 0, 0, { format: 'png' }, (err) => {
-            assert.ok(err);
-            assert.equal(INVALID_FORMAT_ERROR, err.message);
-            assert.equal(400, err.http_status);
-
-            done();
-        });
-    });
+describe('mvt (mapnik)', function () {
+    mvtTest(false);
 });
+
+if (process.env.POSTGIS_VERSION === '2.4') {
+    describe('mvt (pgsql)', function () {
+        mvtTest(true);
+    });
+}
+
+function mvtTest(usePostGIS) {
+    const options = { mvt: { usePostGIS: usePostGIS } };
+
+    describe('vector layergroup', function () {
+        it('should create a single layer map w/o cartocss', function (done) {
+            this.testClient = new TestClient({
+                version: '1.6.0',
+                layers: [
+                    {
+                        type: 'cartodb',
+                        options: {
+                            sql: POINTS_SQL
+                        }
+                    }
+                ]
+            }, options);
+
+            this.testClient.createLayergroup((err, layergroup) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.ok(layergroup);
+                assert.ok(layergroup.layergroupid);
+
+                done();
+            });
+        });
+
+        it('should fail whether layergroup cantains incompatible layers', function (done) {
+            this.testClient = new TestClient({
+                version: '1.6.0',
+                layers: [
+                    {
+                        type: 'cartodb',
+                        options: {
+                            sql: POINTS_SQL
+                        }
+                    },
+                    {
+                        type: 'cartodb',
+                        options: {
+                            sql: POINTS_SQL,
+                            cartocss: '#layer { marker-fill:blue; marker-allow-overlap:true; }',
+                            cartocss_version: '2.0.2',
+                        }
+                    }
+                ]
+            }, options);
+
+            this.testClient.createLayergroup((err) => {
+                assert.ok(err);
+                assert.equal(INCOMPATIBLE_LAYERS_ERROR, err.message);
+                done();
+            });
+        });
+
+        it('should get tile 0/0/0 for a vector layergroup', function (done) {
+            this.testClient = new TestClient({
+                version: '1.6.0',
+                layers: [
+                    {
+                        type: 'cartodb',
+                        options: {
+                            sql: 'select st_setsrid(st_makepoint(0, 0), 4326) as the_geom'
+                        }
+                    }
+                ]
+            }, options);
+
+            this.testClient.getTile(0, 0, 0, { format: 'mvt' }, (err, mvtTile) => {
+                if (err) {
+                    return done(err);
+                }
+
+                var vtile = new mapnik.VectorTile(0, 0, 0);
+                vtile.setData(mvtTile);
+                assert.equal(vtile.painted(), true);
+                assert.equal(vtile.empty(), false);
+                done();
+            });
+        });
+
+        it('should fail when requesting a raster format tile for a vector layergroup', function (done) {
+            this.testClient = new TestClient({
+                version: '1.6.0',
+                layers: [
+                    {
+                        type: 'cartodb',
+                        options: {
+                            sql: POLYGONS_SQL,
+
+                        }
+                    }
+                ]
+            }, options);
+
+            this.testClient.getTile(0, 0, 0, { format: 'png' }, (err) => {
+                assert.ok(err);
+                assert.equal(INVALID_FORMAT_ERROR, err.message);
+                assert.equal(400, err.http_status);
+
+                done();
+            });
+        });
+    });
+};
