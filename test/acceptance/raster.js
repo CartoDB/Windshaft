f87e1c2b5eddf231403c174d5de441b3793925ed
Raul Marin <git@rmr.ninja>
2018-02-12 14:53:54 +0100

    Mapnik metrics: Apply top metrics to all possibilities (formats / error strategies)

--- test/acceptance/raster.js
+++ test/acceptance/raster.js
@@ -96,62 +96,91 @@ describe('raster', function() {
         });
     });
 
-    it("returns Mapnik metrics if requested", function(done) {
 
-        var mapconfig =  {
-            version: '1.2.0',
-            layers: [
-                {
-                    type: 'mapnik',
-                    options: {
-                        sql: "select ST_AsRaster(" +
-                            " ST_MakeEnvelope(-100,-40, 100, 40, 4326), " +
-                            " 1.0, -1.0, '8BUI', 127) as rst",
-                        geom_column: 'rst',
-                        geom_type: 'raster',
-                        cartocss: '#layer { raster-opacity:1.0 }',
-                        cartocss_version: '2.0.1',
+    function onTileErrorStrategyPass(err, tile, headers, stats, format, callback) {
+        callback(err, tile, headers, stats);
+    }
+
+    var ON_TILE_ERROR = [null, onTileErrorStrategyPass];
+    var FORMATS = ["png", "png32", "grid.json"];
+    ON_TILE_ERROR.forEach(function(strat) {
+    FORMATS.forEach(function(format) {
+        it("returns Mapnik metrics if requested " +
+          "[Strategy: " + (strat ? strat.name : "undefined") + ". Format: " + format + ")", function(done) {
+
+            var mapconfig =  {
+                version: '1.2.0',
+                layers: [
+                    {
+                        type: 'mapnik',
+                        options: {
+                            sql: "select 1 as cartodb_id, ST_AsRaster(" +
+                                " ST_MakeEnvelope(-100,-40, 100, 40, 4326), " +
+                                " 1.0, -1.0, '8BUI', 127) as the_geom_webmercator",
+                            geom_column: 'the_geom_webmercator',
+                            geom_type: 'raster',
+                            cartocss: '#layer { raster-opacity:1.0 }',
+                            cartocss_version: '2.0.1'
+                        }
                     }
-                }
-            ]
-        };
+                ]
+            };
 
-        var testClient = new TestClient(mapconfig, { mapnik : { mapnik : { metrics : true } } });
-        testClient.getTile(0, 0, 0, function(err, tile, img, headers, stats) {
-            assert.ok(!err);
-            assert(stats.Mapnik);
-            done();
+            if (format === "grid.json") {
+                delete mapconfig.layers[0].options.geom_column;
+                delete mapconfig.layers[0].options.geom_type;
+                mapconfig.layers[0].options.interactivity = [ 'cartodb_id' ];
+            }
+
+            var testClient = new TestClient(mapconfig, { mapnik : { mapnik : { metrics : true } } }, strat);
+            testClient.getTile(0, 0, 0, {format: format}, function(err, tile, img, headers, stats) {
+                assert.ok(!err);
+                assert(!stats.Mapnik);
+                assert(stats.Mk_Setup);
+                assert(stats.Mk_Render);
+                assert(stats.Mk_FeatNum);
+                done();
+            });
+        });
+
+        it("Doesn't returns Mapnik metrics if set to false " +
+           "[Strategy: " + (strat ? strat.name : "undefined") + ". Format: " +format + ")", function(done) {
+
+            var mapconfig =  {
+                version: '1.2.0',
+                layers: [
+                    {
+                        type: 'mapnik',
+                        options: {
+                            sql: "select 1 as cartodb_id, ST_AsRaster(" +
+                                " ST_MakeEnvelope(-100,-40, 100, 40, 4326), " +
+                                " 1.0, -1.0, '8BUI', 127) as the_geom_webmercator",
+                            geom_column: 'the_geom_webmercator',
+                            geom_type: 'raster',
+                            cartocss: '#layer { raster-opacity:1.0 }',
+                            cartocss_version: '2.0.1'
+                        }
+                    }
+                ]
+            };
+
+            if (format === "grid.json") {
+                delete mapconfig.layers[0].options.geom_column;
+                delete mapconfig.layers[0].options.geom_type;
+                mapconfig.layers[0].options.interactivity = [ 'cartodb_id' ];
+            }
+
+            var testClient = new TestClient(mapconfig, { mapnik : { mapnik : { metrics : false } } }, strat);
+            testClient.getTile(0, 0, 0, {format: format}, function(err, tile, img, headers, stats) {
+                assert.ok(!err);
+                assert(!stats.Mapnik);
+                assert(!stats.Mk_Setup);
+                assert(!stats.Mk_Render);
+                assert(!stats.Mk_FeatNum);
+                done();
+            });
         });
     });
-
-    it("Doesn't returns Mapnik metrics if set to false", function(done) {
-
-        var mapconfig =  {
-            version: '1.2.0',
-            layers: [
-                {
-                    type: 'mapnik',
-                    options: {
-                        sql: "select ST_AsRaster(" +
-                            " ST_MakeEnvelope(-100,-40, 100, 40, 4326), " +
-                            " 1.0, -1.0, '8BUI', 127) as rst",
-                        geom_column: 'rst',
-                        geom_type: 'raster',
-                        cartocss: '#layer { raster-opacity:1.0 }',
-                        cartocss_version: '2.0.1',
-                    }
-                }
-            ]
-        };
-
-        var testClient = new TestClient(mapconfig, { mapnik : { mapnik : { metrics : false } } });
-        testClient.getTile(0, 0, 0, function(err, tile, img, headers, stats) {
-            assert.ok(!err);
-            assert(!stats.Mapnik);
-            done();
-        });
     });
-
-
 });
 
