5184050426263d0983f015ed480c46916c5b8d51
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-10-31 16:12:16 +0100

    Customize project installation depending on Node.js version installed

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+
+usage() {
+    /etc/init.d/postgresql stop
+    echo "Usage: $0 [<nodejs_version>]"
+    exit 1
+}
+
+echo "$0 $1"
+
 # start PostgreSQL
 /etc/init.d/postgresql start
 
 # install dependencies
-npm install
+NODEJS_VERSION=${1-nodejs10}
+
+if [ "$NODEJS_VERSION" = "nodejs10" ];
+then
+    npm install
+elif [ "$NODEJS_VERSION" = "nodejs6" ];
+then
+    npm install -g yarn@0.27.5
+    yarn
+else
+    usage
+fi
 
 # run tests
 npm test
