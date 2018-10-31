ca79bb6ce67e734c59bd5428b8c911572fbcb9bf
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-10-31 13:32:44 +0100

    Use default tag param properly

--- docker-test.sh
+++ docker-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -v `pwd`:/srv carto/${1:-nodejs10-xenial-pg101} bash run_tests_docker.sh && \
+docker run -v `pwd`:/srv carto/${1:-nodejs10-xenial-pg101:postgis-2.4.4.5} bash run_tests_docker.sh && \
     docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
