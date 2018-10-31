6b0fd49d5c8ffff35c78161a2f068bf1621031f5
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-10-31 17:02:37 +0100

    Output Node.js and npm versions

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -11,11 +11,16 @@ echo "$0 $1"
 # start PostgreSQL
 /etc/init.d/postgresql start
 
+echo "Node.js version:"
+node -v
+
 # install dependencies
 NODEJS_VERSION=${1-nodejs10}
 
 if [ "$NODEJS_VERSION" = "nodejs10" ];
 then
+    echo "Node.js version:"
+    npm -v
     npm install
 elif [ "$NODEJS_VERSION" = "nodejs6" ];
 then
