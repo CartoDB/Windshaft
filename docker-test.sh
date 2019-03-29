9425ebbdb996f23541614f9a818875d1f2a017a4
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-03-29 12:19:08 +0100

    Update travis build to run with latest docker image with a specific Node.js version

--- docker-test.sh
+++ docker-test.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-usage() {
-    echo "Usage: $0 [nodejs6|nodejs10]"
-    exit 1
-}
-
-echo "$0 $1"
-
-NODEJS_VERSION=${1-nodejs10}
-
-if [ "$NODEJS_VERSION" = "nodejs10" ];
-then
-    DOCKER='nodejs10-xenial-pg101:postgis-2.4.4.5'
-elif [ "$NODEJS_VERSION" = "nodejs6" ];
-then
-    DOCKER='nodejs6-xenial-pg101:postgis-2.4.4.5'
-else
-    usage
-fi
-
-docker run -v `pwd`:/srv carto/${DOCKER} bash run_tests_docker.sh ${NODEJS_VERSION} && \
-    docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+docker run -e "NODEJS_VERSION=${1}" -v `pwd`:/srv carto/nodejs-xenial-pg101:latest bash run_tests_docker.sh && \
+    docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
