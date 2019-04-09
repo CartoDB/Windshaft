314cca19dc55436a8e786cab397b43811b246b2a
Mario de Frutos Dieguez <mario@defrutos.org>
2019-04-09 19:36:58 +0200

    Test against PG11 and Postgis 2.5

--- docker-test.sh
+++ docker-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -e "NODEJS_VERSION=${1}" -v `pwd`:/srv carto/nodejs-xenial-pg101:latest bash run_tests_docker.sh && \
+docker run -e "NODEJS_VERSION=${1}" -v `pwd`:/srv carto/nodejs-xenial-pg1121:latest bash run_tests_docker.sh && \
     docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
